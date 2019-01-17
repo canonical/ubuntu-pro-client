24f910df96ad3642f46c3ed3842512eb85f60291
Chad Smith <chad.smith@canonical.com>
2019-01-17 13:43:10 -0700

    apt: discover configured apt/auth.conf from apt-config

--- uaclient/apt.py
+++ uaclient/apt.py
@@ -5,11 +5,17 @@ import shutil
 
 from uaclient import util
 
-APT_AUTH_FILE = '/etc/apt/auth.conf'
+APT_CONFIG_AUTH_FILE = 'Dir::Etc::netrc/'
+APT_CONFIG_AUTH_PARTS_DIR = 'Dir::Etc::netrcparts/'
 APT_KEYS_DIR = '/etc/apt/trusted.gpg.d'
 KEYRINGS_DIR = '/usr/share/keyrings'
 APT_METHOD_HTTPS_FILE = '/usr/lib/apt/methods/https'
 CA_CERTIFICATES_FILE = '/usr/sbin/update-ca-certificates'
+
+APT_AUTH_HEADER = """
+# This file is created by ubuntu-advantage-tools and will be updated
+# by subsequent calls to ua attach|detach [entitlement]
+"""
 
 
 class InvalidAPTCredentialsError(RuntimeError):
@@ -64,16 +70,17 @@ def add_auth_apt_repo(repo_filename, repo_url, credentials, keyring_file=None,
     # TODO(Confirm that machine-token or entitlement token provides format)
     # which is supported by /etc/apt/auth.conf
     login, password = credentials.split(':')
-    if os.path.exists(APT_AUTH_FILE):
-        auth_content = util.load_file(APT_AUTH_FILE)
+    apt_auth_file = get_apt_auth_file_from_apt_config()
+    if os.path.exists(apt_auth_file):
+        auth_content = util.load_file(apt_auth_file)
     else:
-        auth_content = ''
+        auth_content = APT_AUTH_HEADER
     _protocol, repo_path = repo_url.split('://')
     auth_content += (
         'machine {repo_path}/ubuntu/ login {login} password'
         ' {password}\n'.format(
             repo_path=repo_path, login=login, password=password))
-    util.write_file(APT_AUTH_FILE, auth_content, mode=0o600)
+    util.write_file(apt_auth_file, auth_content, mode=0o600)
     if keyring_file:
         logging.debug('Copying %s to %s', keyring_file, APT_KEYS_DIR)
         shutil.copy(keyring_file, APT_KEYS_DIR)
@@ -94,16 +101,17 @@ def remove_auth_apt_repo(repo_filename, repo_url, keyring_file=None,
     elif fingerprint:
         util.subp(['apt-key', 'del', fingerprint], capture=True)
     _protocol, repo_path = repo_url.split('://')
-    if os.path.exists(APT_AUTH_FILE):
-        apt_auth = util.load_file(APT_AUTH_FILE)
+    apt_auth_file = get_apt_auth_file_from_apt_config()
+    if os.path.exists(apt_auth_file):
+        apt_auth = util.load_file(apt_auth_file)
         auth_prefix = 'machine {repo_path}/ubuntu/ login'.format(
             repo_path=repo_path)
         content = '\n'.join([
             line for line in apt_auth.split('\n') if auth_prefix not in line])
-        if not content:
-            os.unlink(APT_AUTH_FILE)
+        if not content or content == APT_AUTH_HEADER:
+            os.unlink(apt_auth_file)
         else:
-            util.write_file(APT_AUTH_FILE, content, mode=0o600)
+            util.write_file(apt_auth_file, content, mode=0o600)
 
 
 def add_repo_pinning(apt_preference_file, origin, priority):
@@ -115,3 +123,15 @@ def add_repo_pinning(apt_preference_file, origin, priority):
         'Pin-Priority: ${priority}\n'.format(
             origin=origin, priority=priority, series=series))
     util.write_file(apt_preference_file, content)
+
+
+def get_apt_auth_file_from_apt_config():
+    """Return to patch to the system configured APT auth file."""
+    out, _err = util.subp(
+        ['apt-config', 'shell', 'key', APT_CONFIG_AUTH_PARTS_DIR])
+    if out:  # then auth.conf.d parts is present
+        return out.split("'")[1] + '90ubuntu-advantage'
+    else:    # then use configured /etc/apt/auth.conf
+        out, _err = util.subp(
+            ['apt-config', 'shell', 'key', APT_CONFIG_AUTH_FILE])
+        return out.split("'")[1].rstrip('/')
