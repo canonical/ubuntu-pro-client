b28afb2dc84bd86c936b1d45839791686d669ed2
Daniel Watkins <daniel.watkins@canonical.com>
2019-03-28 16:54:14 -0400

    testing/helpers: drop our NIH log capturing code
    It is currently unused, and any new tests requiring log capture should
    use the pytest caplog fixture.

--- uaclient/testing/helpers.py
+++ uaclient/testing/helpers.py
@@ -1,6 +1,4 @@
 import functools
-import io
-import logging
 import os
 import shutil
 import tempfile
@@ -8,34 +6,6 @@ import unittest
 
 
 class TestCase(unittest.TestCase):
-
-    with_logs = False
-
-    def setUp(self):
-        super().setUp()
-        if self.with_logs:
-            # Create a log handler so unit tests can search expected logs.
-            self.logger = logging.getLogger()
-            self._logs = io.StringIO()
-            formatter = logging.Formatter('%(levelname)s: %(message)s')
-            handler = logging.StreamHandler(self._logs)
-            handler.setFormatter(formatter)
-            self.old_handlers = self.logger.handlers
-            self.logger.handlers = [handler]
-
-    def tearDown(self):
-        if self.with_logs:
-            # Remove the handler we setup
-            logging.getLogger().handlers = self.old_handlers
-        super().tearDown()
-
-    @property
-    def logs(self):
-        """Return the contents of logs written during the unit test run."""
-        if not self.with_logs:
-            raise AssertionError(
-                'Cannot reference test.logs when with_logs == False')
-        return self._logs.getvalue()
 
     def tmp_dir(self, dir=None):
         if dir is None:
