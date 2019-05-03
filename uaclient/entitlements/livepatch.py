fa5b89ac59f17b94e3e766fa285c8c0de804a830
Chad Smith <chad.smith@canonical.com>
2019-05-03 13:19:52 -0600

    livepatch: handle deltas in caCert, remoteServer and resourceToken
    Also re-order config processing to process caCerts deltas before
    remoteServer because a remoteServer change triggers a PUT to the
    new livepatch server to check. If bew certs were not matching the new
    remoteServer, then uaclient would get a config change non-zero exit
    code.
    
    Fixes: #386

--- uaclient/entitlements/livepatch.py
+++ uaclient/entitlements/livepatch.py
@@ -4,6 +4,11 @@ from uaclient.entitlements import base
 from uaclient import status
 from uaclient import util
 
+try:
+    from typing import Any, Dict  # noqa: F401
+except ImportError:
+    # typing isn't available on trusty, so ignore its absence
+    pass
 
 ERROR_MSG_MAP = {
     'Unknown Auth-Token': 'Invalid Auth-Token provided to livepatch.',
@@ -92,35 +97,50 @@ class LivepatchEntitlement(base.UAEntitlement):
                 print(msg)
                 logging.error(msg)
                 return False
+        return self.setup_livepatch_config(
+            process_directives=True, process_token=True)
+
+    def setup_livepatch_config(
+            self, process_directives: bool = True,
+            process_token: bool = True) -> bool:
+        """Processs configuration setup for livepatch directives.
+
+        :param process_directives: Boolean set True when directives should be
+            processsed.
+        :param process_token: Boolean set True when token should be
+            processsed.
+        """
         entitlement_cfg = self.cfg.entitlements.get(self.name)
-        try:
-            process_directives(entitlement_cfg)
-        except util.ProcessExecutionError as e:
-            msg = 'Unable to configure Livepatch: ' + str(e)
-            print(msg)
-            logging.error(msg)
-            return False
-        livepatch_token = entitlement_cfg.get('resourceToken')
-        if not livepatch_token:
-            logging.debug(
-                'No specific resourceToken present. Using machine token as'
-                ' %s credentials', self.title)
-            livepatch_token = self.cfg.machine_token['machineToken']
-        try:
-            util.subp(['/snap/bin/canonical-livepatch', 'enable',
-                       livepatch_token],
-                      capture=True)
-        except util.ProcessExecutionError as e:
-            msg = 'Unable to enable Livepatch: '
-            for error_message, print_message in ERROR_MSG_MAP.items():
-                if error_message in str(e):
-                    msg += print_message
-                    break
-            if msg == 'Unable to enable Livepatch: ':
-                msg += str(e)
-            print(msg)
-            return False
-        print('Canonical livepatch enabled.')
+        if process_directives:
+            try:
+                process_config_directives(entitlement_cfg)
+            except util.ProcessExecutionError as e:
+                msg = 'Unable to configure Livepatch: ' + str(e)
+                print(msg)
+                logging.error(msg)
+                return False
+        if process_token:
+            livepatch_token = entitlement_cfg.get('resourceToken')
+            if not livepatch_token:
+                logging.debug(
+                    'No specific resourceToken present. Using machine token as'
+                    ' %s credentials', self.title)
+                livepatch_token = self.cfg.machine_token['machineToken']
+            try:
+                util.subp(['/snap/bin/canonical-livepatch', 'enable',
+                           livepatch_token],
+                          capture=True)
+            except util.ProcessExecutionError as e:
+                msg = 'Unable to enable Livepatch: '
+                for error_message, print_message in ERROR_MSG_MAP.items():
+                    if error_message in str(e):
+                        msg += print_message
+                        break
+                if msg == 'Unable to enable Livepatch: ':
+                    msg += str(e)
+                print(msg)
+                return False
+            print('Canonical livepatch enabled.')
         return True
 
     def disable(self, silent=False, force=False):
@@ -160,8 +180,46 @@ class LivepatchEntitlement(base.UAEntitlement):
             operational_status = (status.INACTIVE, str(e))
         return operational_status
 
+    def process_contract_deltas(
+            self, orig_access: 'Dict[str, Any]',
+            deltas: 'Dict[str, Any]', allow_enable: bool = False) -> bool:
+        """Process any contract access deltas for this entitlement.
 
-def process_directives(cfg):
+        :param orig_access: Dictionary containing the original
+            resourceEntitlement access details.
+        :param deltas: Dictionary which contains only the changed access keys
+        and values.
+        :param allow_enable: Boolean set True if allowed to perform the enable
+            operation. When False, a message will be logged to inform the user
+            about the recommended enabled service.
+
+        :return: True when delta operations are processed; False when noop.
+        """
+        if super().process_contract_deltas(orig_access, deltas, allow_enable):
+            return True  # Already processed parent class deltas
+        op_status, _details = self.operational_status()
+        if op_status != status.ACTIVE:
+            return True  # only operate on changed directives when ACTIVE
+        delta_entitlement = deltas.get('entitlement', {})
+        delta_directives = delta_entitlement.get('directives', {})
+        supported_deltas = set(['caCerts', 'remoteServer'])
+        process_directives = bool(
+            supported_deltas.intersection(delta_directives))
+        process_token = bool(deltas.get('resourceToken', False))
+        if any([process_directives, process_token]):
+            logging.info(
+                "Updating '%s' on changed directives." % self.name)
+            if process_token:
+                # Disable livepatch before re-enable
+                util.subp(['/snap/bin/canonical-livepatch', 'disable'],
+                          capture=True)
+            return self.setup_livepatch_config(
+                process_directives=process_directives,
+                process_token=process_token)
+        return True
+
+
+def process_config_directives(cfg):
     """Process livepatch configuration directives.
 
     @raises: ProcessExecutionError if unable to configure livepatch.
@@ -169,13 +227,13 @@ def process_directives(cfg):
     if not cfg:
         return
     directives = cfg.get('entitlement', {}).get('directives', {})
+    ca_certs = directives.get('caCerts')
+    if ca_certs:
+        util.subp(['/snap/bin/canonical-livepatch', 'config',
+                   'ca-certs=%s' % ca_certs], capture=True)
     remote_server = directives.get('remoteServer', '')
     if remote_server.endswith('/'):
         remote_server = remote_server[:-1]
     if remote_server:
         util.subp(['/snap/bin/canonical-livepatch', 'config',
                    'remote-server=%s' % remote_server], capture=True)
-    ca_certs = directives.get('caCerts')
-    if ca_certs:
-        util.subp(['/snap/bin/canonical-livepatch', 'config',
-                   'ca-certs=%s' % ca_certs], capture=True)
