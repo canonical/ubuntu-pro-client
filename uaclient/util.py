181273cb5602bed3d1ccedf579f47ede145845cb
Daniel Watkins <daniel.watkins@canonical.com>
2019-05-27 09:48:17 -0400

    util: add type annotations to _subp

--- uaclient/util.py
+++ uaclient/util.py
@@ -167,7 +167,8 @@ def readurl(url: str, data: 'Optional[bytes]' = None,
     return content, resp.headers
 
 
-def _subp(args, rcs=None, capture=False):
+def _subp(args: 'Sequence[str]', rcs: 'Optional[List[int]]' = None,
+          capture: bool = False) -> 'Tuple[str, str]':
     """Run a command and return a tuple of decoded stdout, stderr.
 
     @param subp: A list of arguments to feed to subprocess.Popen
