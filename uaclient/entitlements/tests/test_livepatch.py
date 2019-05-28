188760f219123c25a44f4ee764ff7386c6773046
Daniel Watkins <daniel.watkins@canonical.com>
2019-05-28 10:52:35 -0400

    test_livepatch: remove unnecessary log level workaround
    caplog_text now logs at INFO by default, so we no longer need to filter
    out debug log messages.
    
    (Also remove the TODO that this addresses.)

--- uaclient/entitlements/tests/test_livepatch.py
+++ uaclient/entitlements/tests/test_livepatch.py
@@ -316,10 +316,7 @@ class TestLivepatchEntitlementEnable:
         """When can_enable returns False enable returns False."""
         with mock.patch('sys.stdout', new_callable=StringIO) as m_stdout:
             assert not entitlement.enable()
-        info_level_logs = [  # see uaclient/conftest.py
-            line for line in caplog_text().splitlines()
-            if 'DEBUG' not in line]
-        assert [] == info_level_logs
+        assert '' == caplog_text()
         assert '' == m_stdout.getvalue()  # No additional prints
         assert [mock.call(silent=mock.ANY)] == m_can_enable.call_args_list
 
