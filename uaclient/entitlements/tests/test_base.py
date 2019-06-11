ee4edf9cb1558880728dbb535c4b0e0b30494ab5
Daniel Watkins <daniel.watkins@canonical.com>
2019-06-11 15:12:20 -0400

    base: don't message about disabling already-disabled entitlements
    This is inaccurate and confusing.
    
    Fixes #599

--- uaclient/entitlements/tests/test_base.py
+++ uaclient/entitlements/tests/test_base.py
@@ -255,7 +255,8 @@ class TestUaEntitlement:
           {'entitlement': {'entitled': False}})  # transition to unentitled
          ))
     def test_process_contract_deltas_clean_cache_on_inactive_unentitled(
-            self, concrete_entitlement_factory, orig_access, delta):
+            self, concrete_entitlement_factory, orig_access, delta,
+            caplog_text):
         """Only clear cache when deltas transition inactive to unentitled."""
         entitlement = concrete_entitlement_factory(
             entitled=True,
@@ -267,6 +268,12 @@ class TestUaEntitlement:
         # Cache was cleaned
         assert None is entitlement.cfg.read_cache(
             'machine-access-testconcreteentitlement')
+        # If an entitlement is disabled, we don't need to tell the user
+        # anything about it becoming unentitled
+        # (FIXME: Something on bionic means that DEBUG log lines are being
+        # picked up by caplog_text(), so work around that here)
+        assert [] == [line for line in caplog_text().splitlines()
+                      if 'DEBUG' not in line]
 
     @pytest.mark.parametrize(
         'orig_access,delta',
