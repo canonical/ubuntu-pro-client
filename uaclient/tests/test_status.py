67fc433557985fce2100a91d79140b4685117c3c
Daniel Watkins <daniel.watkins@canonical.com>
2019-06-27 14:08:53 -0400

    test_status: add unit tests for header changes

--- uaclient/tests/test_status.py
+++ uaclient/tests/test_status.py
@@ -46,3 +46,35 @@ class TestFormatTabular:
             support_level if not expected_colour else
             expected_colour + support_level + TxtColor.ENDC)
         assert expected_string in tabular_output
+
+    @pytest.mark.parametrize('origin', ['free', 'not-free'])
+    def test_header_alignment(self, origin, status_dict_attached):
+        status_dict_attached['origin'] = origin
+        tabular_output = format_tabular(status_dict_attached)
+        colon_idx = None
+        for line in tabular_output.splitlines():
+            if ':' not in line:
+                # This isn't a header line
+                continue
+            if colon_idx is None:
+                # This is the first header line, record where the colon is
+                colon_idx = line.index(':')
+                continue
+            # Ensure that the colon in this line is aligned with previous ones
+            assert line.index(':') == colon_idx
+
+    @pytest.mark.parametrize('origin,expected_headers', [
+        ('free', ('Account', 'Subscription')),
+        ('not-free', ('Account', 'Subscription', 'Valid until',
+                      'Technical support level')),
+    ])
+    def test_correct_header_keys_included(
+            self, origin, expected_headers, status_dict_attached):
+        status_dict_attached['origin'] = origin
+
+        tabular_output = format_tabular(status_dict_attached)
+
+        headers = [line.split(':')[0].strip()
+                   for line in tabular_output.splitlines()
+                   if ':' in line]
+        assert list(expected_headers) == headers
