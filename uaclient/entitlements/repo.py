2d3f58a3b71461e77db5aa4cda7bb1a84e2649e1
Chad Smith <chad.smith@canonical.com>
2019-08-29 21:55:37 -0600

    demo: add trusty platform/cloud detection and upstart

--- uaclient/entitlements/repo.py
+++ uaclient/entitlements/repo.py
@@ -11,7 +11,6 @@ except ImportError:
 
 
 from uaclient import apt
-from uaclient import defaults
 from uaclient import exceptions
 from uaclient.entitlements import base
 from uaclient import status
