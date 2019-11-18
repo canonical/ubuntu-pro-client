351760637662d875b4655cf1fe530eb01d032c99
Chad Smith <chad.smith@canonical.com>
2019-11-18 11:11:17 -0700

    normalize get_cloud_type on trusty

--- uaclient/clouds/identity.py
+++ uaclient/clouds/identity.py
@@ -8,11 +8,16 @@ from uaclient import util
 CLOUDINIT_RESULT_FILE = "/var/lib/cloud/data/result.json"
 
 
+# Mapping of datasource names to cloud-id responses. Trusty compat with Xenial+
+DATASOURCE_TO_CLOUD_ID = {"ec2": "aws"}
+
+
 @util.retry(FileNotFoundError, [1, 2])
 def get_cloud_type_from_result_file(result_file=CLOUDINIT_RESULT_FILE) -> str:
     result = json.loads((util.load_file(result_file)))
     dsname = result["v1"]["datasource"].split()[0].lower()
-    return dsname.replace("datasource", "")
+    dsname = dsname.replace("datasource", "")
+    return DATASOURCE_TO_CLOUD_ID.get(dsname, dsname)
 
 
 def get_cloud_type() -> str:
@@ -30,10 +35,7 @@ def get_cloud_type() -> str:
 def cloud_instance_factory() -> clouds.UAPremiumCloudInstance:
     from uaclient.clouds import aws
 
-    cloud_instance_map = {
-        "aws": aws.UAPremiumAWSInstance,
-        "ec2": aws.UAPremiumAWSInstance,
-    }
+    cloud_instance_map = {"aws": aws.UAPremiumAWSInstance}
 
     cloud_type = get_cloud_type()
     if not cloud_type:
