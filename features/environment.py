a8c9868729ffec931e5715ba947c9f16a6304a48
Patricia Domingues <patricia.domingues@canonical.com>
2020-05-14 10:58:04 -0300

    read new travis vars

--- features/environment.py
+++ features/environment.py
@@ -104,6 +104,9 @@ def before_all(context: Context) -> None:
     context.series_reuse_image = ""
     context.reuse_container = {}
     context.config = UAClientBehaveConfig.from_environ()
+    print ('\n\ntest build os.env travis env: ', os.environ['TEST_BUILD_ENV'])
+    print ('test build: ', os.environ['TEST_BUILD_PUSH'])
+    print ('\n\n\n\')
     if context.config.reuse_image:
         series = lxc_get_series(context.config.reuse_image, image=True)
         if series is not None:
