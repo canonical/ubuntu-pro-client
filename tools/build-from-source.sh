33171a0fb15f375665cc22d38f5d11252ba9ec3f
Patricia Domingues <patricia.domingues@canonical.com>
2020-05-20 16:28:28 -0300

    remove extract from the script

--- tools/build-from-source.sh
+++ tools/build-from-source.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 set -o xtrace
 apt-get update
-tar -xzvf /tmp/pr_source.tar.gz --directory /tmp
+apt-get install make
 cd /tmp
 ls -lh
 lsb_release -a
 cd /tmp/ubuntu-advantage-client
-apt-get install make
 make deps
 dpkg-buildpackage -us -uc
 ls /tmp |grep ubuntu-advantage-tools
