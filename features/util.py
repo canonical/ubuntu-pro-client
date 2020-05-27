5007cbd3d8dd8ef72f88fb04a9447cb7854a336d
Patricia Domingues <patricia.domingues@canonical.com>
2020-05-27 20:53:25 -0300

    move back full path for .deb file
    lxc file pull does not support wildcards
    I've tested it and it did not work
    then hit into this issue
    https://github.com/lxc/lxd/issues/3523
    
    ```
    root@behave-image-pre-build-trusty-1590622166510319:/tmp# ls -lh
    total 3.2M
    drwxrwxr-x  16 ubuntu ubuntu 4.0K May 27 23:32 ubuntu-advantage-client
    -rw-r--r--   1 root   root   6.9K May 27 23:32 ubuntu-advantage-pro_20.4_amd64.deb
    -rw-r--r--   1 root   root   1014 May 27 23:31 ubuntu-advantage-tools_20.4.dsc
    -rw-r--r--   1 root   root   167K May 27 23:31 ubuntu-advantage-tools_20.4.tar.gz
    -rw-r--r--   1 root   root   1.8K May 27 23:32 ubuntu-advantage-tools_20.4_amd64.changes
    -rw-r--r--   1 root   root    58K May 27 23:32 ubuntu-advantage-tools_20.4_amd64.deb
    root@behave-image-pre-build-trusty-1590622166510319:/tmp#
    
    $ lxc file pull behave-image-pre-build-trusty-1590622166510319/tmp/ubuntu-advantage-tools*.deb .
    Error: not found
    
    $ lxc file pull behave-image-pre-build-trusty-1590622166510319/tmp/ubuntu-advantage-tools_20.4_amd64.deb .
    $ ls |grep deb
    ubuntu-advantage-tools_20.4_amd64.deb
    
    ───────────────────────────────────────────────────────────────────

--- features/util.py
+++ features/util.py
@@ -210,4 +210,4 @@ def lxc_build_deb(
     )
     print ("\n\nPull .deb from the instance to travis VM")
     subprocess.run(["lxc", "exec", container_name, "--", "ls", "-lh" , "/tmp"])
-    subprocess.run(["lxc", "file", "pull", container_name+'/tmp/ubuntu-advantage-tools*.deb', output_deb_file])
+    subprocess.run(["lxc", "file", "pull", container_name+'/tmp/ubuntu-advantage-tools_20.4_amd64.deb', output_deb_file])
