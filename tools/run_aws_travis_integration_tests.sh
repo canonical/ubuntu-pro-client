547d26d4b5e5a1ce032215adf86317da8aa3d68a
Lucas Moura <lucas.moura@canonical.com>
2020-10-07 11:05:37 -0300

    Refactor travis file

---
+++ tools/run_aws_travis_integration_tests.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+source tools/base_travis_integration_tests.sh
+
+copy_deb_packages
+
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+  BUILD_PR=0
+else
+  create_pr_tar_file
+fi
+
+UACLIENT_BEHAVE_BUILD_PR=${BUILD_PR} make test
+
+for key_name in `ls *pem`; do
+    K=${key_name/ec2-/};
+    python3 server-test-scripts/ubuntu-advantage-client/ec2_cleanup.py -t ${K%.pem} || true
+done;
---
+++ tools/run_lxd_travis_integration_tests.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+source tools/base_travis_integration_tests.sh
+
+install_lxd() {
+  sudo snap install lxd
+  sudo lxd init --auto
+  sudo usermod -a -G lxd $USER
+}
+
+copy_deb_packages
+
+if [ "$TRAVIS_EVENT_TYPE" == "cron" ]; then
+  BUILD_PR=0
+else
+  create_pr_tar_file
+fi
+
+# Because we are using dist:bionic for the travis host, we need
+# to remove the lxd deb-installed package to avoid
+# confusion over lxd versions
+remove_lxd
+
+install_lxd
+sg lxd -c "UACLIENT_BEHAVE_BUILD_PR=${BUILD_PR} make test"
