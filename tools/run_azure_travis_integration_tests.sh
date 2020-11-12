3fc8aa6e597341f3d6cc548dd4597358d6d1ad1e
Lucas Moura <lucas.moura@canonical.com>
2020-11-12 15:46:39 -0300

    Install pycloudlib for azure jobs only

--- tools/run_azure_travis_integration_tests.sh
+++ tools/run_azure_travis_integration_tests.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 source tools/base_travis_integration_tests.sh
 
+install_pycloudlib() {
+    git clone https://github.com/canonical/pycloudlib.git
+    cd pycloudlib
+    pip install -r requirements.txt
+    cd ..
+}
+
 copy_deb_packages
 
 if [ "$TRAVIS_EVENT_TYPE" = "cron" ] && [ "$TRAVIS_BRANCH" = "master" ]; then
@@ -11,5 +18,6 @@ fi
 
 UACLIENT_BEHAVE_BUILD_PR=${BUILD_PR} make test
 
+install_pycloudlib
 echo $TRAVIS_JOB_NUMBER
 python3 server-test-scripts/ubuntu-advantage-client/azure_cleanup.py --suffix-tag ${TRAVIS_JOB_NUMBER/./-} --client-id ${UACLIENT_BEHAVE_AZ_CLIENT_ID} --client-secret ${UACLIENT_BEHAVE_AZ_CLIENT_SECRET} --subscription-id ${UACLIENT_BEHAVE_AZ_SUBSCRIPTION_ID} --tenant-id ${UACLIENT_BEHAVE_AZ_TENANT_ID} || true
