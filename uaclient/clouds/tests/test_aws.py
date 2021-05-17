41c21303ae7d3454c72de880d783a95dc1b0ba03
Chad Smith <chad.smith@canonical.com>
2021-05-16 21:31:12 -0600

    pro: obtain AWS IMDSv2 API token before trying to grab pkcs7 doc
    Avoid 401 Unauthorized errors from IMDSv2 API on AWS by
    obtaining an API token from latest/api/token.
    
    The Unauthorized errors result in inability to auto-attach on
    Ubuntu PRO instances which are configured at launch to only
    allow IMDSv2 traffic.
    
    Use this token with any GET calls to read the pkcs7 metadata
    by providing the API token as a header X-aws-ec2-metadata-token.
    
    Some regions may not have IMDSv2 support, in those cases fallback
    to using no token.
    
    Also redact this IMDSv2 token from GET and PUT response logs.
    
    Fixes: #1608

--- uaclient/clouds/tests/test_aws.py
+++ uaclient/clouds/tests/test_aws.py
@@ -5,7 +5,12 @@ from urllib.error import HTTPError
 
 import pytest
 
-from uaclient.clouds.aws import UAAutoAttachAWSInstance
+from uaclient.clouds.aws import (
+    AWS_TOKEN_PUT_HEADER,
+    AWS_TOKEN_REQ_HEADER,
+    AWS_TOKEN_TTL_SECONDS,
+    UAAutoAttachAWSInstance,
+)
 
 M_PATH = "uaclient.clouds.aws."
 
@@ -16,13 +21,95 @@ class TestUAAutoAttachAWSInstance:
         assert "aws" == instance.cloud_type
 
     @mock.patch(M_PATH + "util.readurl")
+    def test__get_imds_v2_token_headers_none_on_404(self, readurl):
+        """A 404 on private AWS regions indicates lack IMDSv2 support."""
+        readurl.side_effect = HTTPError(
+            "http://me", 404, "No IMDSv2 support", None, BytesIO()
+        )
+        instance = UAAutoAttachAWSInstance()
+        assert None is instance._get_imds_v2_token_headers()
+        assert "IMDSv1" == instance._api_token
+        # No retries on 404. It is a permanent indication of no IMDSv2 support.
+        instance._get_imds_v2_token_headers()
+        assert 1 == readurl.call_count
+
+    @mock.patch(M_PATH + "util.readurl")
+    def test__get_imds_v2_token_headers_caches_response(self, readurl):
+        """Return API token headers for IMDSv2 access. Response is cached."""
+        instance = UAAutoAttachAWSInstance()
+        url = "http://169.254.169.254/latest/api/token"
+        readurl.return_value = "somebase64token==", {"header": "stuff"}
+        assert {
+            AWS_TOKEN_PUT_HEADER: "somebase64token=="
+        } == instance._get_imds_v2_token_headers()
+        instance._get_imds_v2_token_headers()
+        assert "somebase64token==" == instance._api_token
+        assert [
+            mock.call(
+                url,
+                method="PUT",
+                headers={AWS_TOKEN_REQ_HEADER: AWS_TOKEN_TTL_SECONDS},
+            )
+        ] == readurl.call_args_list
+
+    @pytest.mark.parametrize("caplog_text", [logging.DEBUG], indirect=True)
+    @pytest.mark.parametrize("fail_count,exception", ((3, False), (4, True)))
+    @mock.patch(M_PATH + "util.time.sleep")
+    @mock.patch(M_PATH + "util.readurl")
+    def test_retry_backoff_on__get_imds_v2_token_headers_caches_response(
+        self, readurl, sleep, fail_count, exception, caplog_text
+    ):
+        """Retry backoff before failing _get_imds_v2_token_headers."""
+
+        def fake_someurlerrors(url, method=None, headers=None):
+            if readurl.call_count <= fail_count:
+                raise HTTPError(
+                    "http://me",
+                    700 + readurl.call_count,
+                    "funky error msg",
+                    None,
+                    BytesIO(),
+                )
+            return "base64token==", {"header": "stuff"}
+
+        readurl.side_effect = fake_someurlerrors
+        instance = UAAutoAttachAWSInstance()
+        if exception:
+            with pytest.raises(HTTPError) as excinfo:
+                instance._get_imds_v2_token_headers()
+            assert 704 == excinfo.value.code
+        else:
+            assert {
+                AWS_TOKEN_PUT_HEADER: "base64token=="
+            } == instance._get_imds_v2_token_headers()
+
+        expected_sleep_calls = [mock.call(1), mock.call(2), mock.call(5)]
+        assert expected_sleep_calls == sleep.call_args_list
+        expected_logs = [
+            "HTTP Error 701: funky error msg Retrying 3 more times.",
+            "HTTP Error 702: funky error msg Retrying 2 more times.",
+            "HTTP Error 703: funky error msg Retrying 1 more times.",
+        ]
+        logs = caplog_text()
+        for log in expected_logs:
+            assert log in logs
+
+    @mock.patch(M_PATH + "util.readurl")
     def test_identity_doc_from_aws_url_pkcs7(self, readurl):
         """Return pkcs7 content from IMDS as AWS' identity doc"""
         readurl.return_value = "pkcs7WOOT!==", {"header": "stuff"}
         instance = UAAutoAttachAWSInstance()
         assert {"pkcs7": "pkcs7WOOT!=="} == instance.identity_doc
         url = "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7"
-        assert [mock.call(url)] == readurl.call_args_list
+        token_url = "http://169.254.169.254/latest/api/token"
+        assert [
+            mock.call(
+                token_url,
+                method="PUT",
+                headers={AWS_TOKEN_REQ_HEADER: AWS_TOKEN_TTL_SECONDS},
+            ),
+            mock.call(url, headers={AWS_TOKEN_PUT_HEADER: "pkcs7WOOT!=="}),
+        ] == readurl.call_args_list
 
     @pytest.mark.parametrize("caplog_text", [logging.DEBUG], indirect=True)
     @pytest.mark.parametrize("fail_count,exception", ((3, False), (4, True)))
@@ -33,8 +120,11 @@ class TestUAAutoAttachAWSInstance:
     ):
         """Retry backoff is attempted before failing to get AWS.identity_doc"""
 
-        def fake_someurlerrors(url):
-            if readurl.call_count <= fail_count:
+        def fake_someurlerrors(url, method=None, headers=None):
+            # due to _get_imds_v2_token_headers
+            if "latest/api/token" in url:
+                return "base64token==", {"header": "stuff"}
+            if readurl.call_count <= fail_count + 1:
                 raise HTTPError(
                     "http://me",
                     700 + readurl.call_count,
@@ -49,16 +139,16 @@ class TestUAAutoAttachAWSInstance:
         if exception:
             with pytest.raises(HTTPError) as excinfo:
                 instance.identity_doc
-            assert 704 == excinfo.value.code
+            assert 705 == excinfo.value.code
         else:
             assert {"pkcs7": "pkcs7WOOT!=="} == instance.identity_doc
 
         expected_sleep_calls = [mock.call(1), mock.call(2), mock.call(5)]
         assert expected_sleep_calls == sleep.call_args_list
         expected_logs = [
-            "HTTP Error 701: funky error msg Retrying 3 more times.",
-            "HTTP Error 702: funky error msg Retrying 2 more times.",
-            "HTTP Error 703: funky error msg Retrying 1 more times.",
+            "HTTP Error 702: funky error msg Retrying 3 more times.",
+            "HTTP Error 703: funky error msg Retrying 2 more times.",
+            "HTTP Error 704: funky error msg Retrying 1 more times.",
         ]
         logs = caplog_text()
         for log in expected_logs:
