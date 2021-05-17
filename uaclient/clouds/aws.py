c4c2429c68b12394628e5666c28114893beb3d74
Chad Smith <chad.smith@canonical.com>
2021-05-16 21:31:12 -0600

    pro: obtain AWS IMDSv2 API token before trying to grab pkcs7 doc
    Avoid 401 Unauthorized errors from IMDSv2 API on AWS by
    obtaining an API token from latest/api/token.
    
    The Unauthorized errors result in inability to auto-attach on
    Ubuntu PRO instances which are configured at launch to only
    allow IMDSv2 traffic.
    
    Use this token with any GET calls to read the pkcs7 metadata
    by providing the API token as a header X-aws-ec2-metadata-token.
    
    Some regions may not have IMDSv2 support, in those cases fallback
    to using no token.
    
    Also redact this IMDSv2 token from GET and PUT response logs.
    
    Fixes: #1608

--- uaclient/clouds/aws.py
+++ uaclient/clouds/aws.py
@@ -10,21 +10,50 @@ from uaclient.clouds import AutoAttachCloudInstance
 from uaclient import util
 
 
+IMDS_V2_TOKEN_URL = "http://169.254.169.254/latest/api/token"
 IMDS_URL = "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7"
 SYS_HYPERVISOR_PRODUCT_UUID = "/sys/hypervisor/uuid"
 DMI_PRODUCT_SERIAL = "/sys/class/dmi/id/product_serial"
 DMI_PRODUCT_UUID = "/sys/class/dmi/id/product_uuid"
 
+AWS_TOKEN_TTL_SECONDS = "21600"
+AWS_TOKEN_PUT_HEADER = "X-aws-ec2-metadata-token"
+AWS_TOKEN_REQ_HEADER = AWS_TOKEN_PUT_HEADER + "-ttl-seconds"
+
 
 class UAAutoAttachAWSInstance(AutoAttachCloudInstance):
+
+    _api_token = None
 
     # mypy does not handle @property around inner decorators
     # https://github.com/python/mypy/issues/1362
     @property  # type: ignore
     @util.retry(HTTPError, retry_sleeps=[1, 2, 5])
     def identity_doc(self) -> "Dict[str, Any]":
-        response, _headers = util.readurl(IMDS_URL)
+        headers = self._get_imds_v2_token_headers()
+        response, _headers = util.readurl(IMDS_URL, headers=headers)
         return {"pkcs7": response}
+
+    @util.retry(HTTPError, retry_sleeps=[1, 2, 5])
+    def _get_imds_v2_token_headers(self):
+        if self._api_token == "IMDSv1":
+            return None
+        elif self._api_token:
+            return {AWS_TOKEN_PUT_HEADER: self._api_token}
+        try:
+            response, _headers = util.readurl(
+                IMDS_V2_TOKEN_URL,
+                method="PUT",
+                headers={AWS_TOKEN_REQ_HEADER: AWS_TOKEN_TTL_SECONDS},
+            )
+        except HTTPError as e:
+            if e.code == 404:
+                self._api_token = "IMDSv1"
+                return None
+            else:
+                raise
+        self._api_token = response
+        return {AWS_TOKEN_PUT_HEADER: self._api_token}
 
     @property
     def cloud_type(self) -> str:
