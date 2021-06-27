0c92bd4e38d1b5ef7af413d1d4ce6b55af347aea
Chad Smith <chad.smith@canonical.com>
2021-06-27 12:05:11 -0600

    livepatch: add unconfigure_livepatch_proxy and snap_proxy

--- uaclient/snap.py
+++ uaclient/snap.py
@@ -1,5 +1,7 @@
 from typing import List, Optional
 
+import logging
+
 from uaclient import apt, status, util
 
 SNAP_CMD = "/usr/bin/snap"
@@ -14,9 +16,9 @@ def is_installed() -> bool:
 
 
 def configure_snap_proxy(
-    http_proxy: Optional[str],
-    https_proxy: Optional[str],
-    snap_retries: Optional[List[float]] = None,
+    http_proxy: Optional[str] = None,
+    https_proxy: Optional[str] = None,
+    retry_sleeps: Optional[List[float]] = None,
 ) -> None:
     """
     Configure snap to use http and https proxies.
@@ -25,23 +27,58 @@ def configure_snap_proxy(
                        not be configured
     :param https_proxy: https proxy to be used by snap. If None, it will
                         not be configured
-    :@param snap_retries: Optional list of sleep lengths to apply between
-                          snap calls
+    :param retry_sleeps: Optional list of sleep lengths to apply between
+        retries. Specifying a list of [0.5, 1] tells subp to retry twice
+        on failure; sleeping half a second before the first retry and 1 second
+        before the second retry.
     """
+    if not util.which(SNAP_CMD):
+        logging.debug(
+            "Skipping configure snap proxy. {} does not exist.".format(
+                SNAP_CMD
+            )
+        )
+        return
+
     if http_proxy or https_proxy:
         print(status.MESSAGE_SETTING_SERVICE_PROXY.format(service="snap"))
 
     if http_proxy:
         util.subp(
             ["snap", "set", "system", "proxy.http={}".format(http_proxy)],
-            retry_sleeps=snap_retries,
+            retry_sleeps=retry_sleeps,
         )
 
     if https_proxy:
         util.subp(
             ["snap", "set", "system", "proxy.https={}".format(https_proxy)],
-            retry_sleeps=snap_retries,
+            retry_sleeps=retry_sleeps,
         )
+
+
+def unconfigure_snap_proxy(
+    protocol_type: str, retry_sleeps: Optional[List[float]] = None
+) -> None:
+    """
+    Unset snap configuration settings for http and https proxies.
+
+    :param protocol_type: String either http or https
+    :param retry_sleeps: Optional list of sleep lengths to apply between
+        retries. Specifying a list of [0.5, 1] tells subp to retry twice
+        on failure; sleeping half a second before the first retry and 1 second
+        before the second retry.
+    """
+    if not util.which(SNAP_CMD):
+        logging.debug(
+            "Skipping unconfigure snap proxy. {} does not exist.".format(
+                SNAP_CMD
+            )
+        )
+        return
+    util.subp(
+        ["snap", "unset", "system", "proxy.{}".format(protocol_type)],
+        retry_sleeps=retry_sleeps,
+    )
 
 
 def get_config_option_value(key: str) -> Optional[str]:
