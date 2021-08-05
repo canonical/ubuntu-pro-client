4cd20b3f2326f9ea3df8af99ece1dc042fa292c1
Chad Smith <chad.smith@canonical.com>
2021-08-05 09:54:25 -0600

    testing: add overlay for any serviceclient API response
    To enable development testing of features that are not yet
    available on the contract server or security API endpoints,
    We can now provide API response overrides from a file on disk for any
    matched URL.
    
    uaclient.conf can add
    features:
      serviceclient_url_responses: <some_json_file>
    
    The represented some_json_file will contain a JSON dict keyed
    by full URL path.
    
    Each value for the URL key will be a list containing response dicts
    with a HTTP "code" key and a "response" key.
    Any servicelicent URL requests to URLs matching the
    serviceclient_url_responses will be return the faked responses
    
    Here are a few examples:
    - Fake available resources URL
    {
      "https://contracts.canonincal.com/v1/resouces": [
        {"code": 200, "response": {"resources": ["livepatch", "esm-apps"]}}
      ]
    }
    
    -Fake URLerror from available resources endpoint
    {
      "https://contracts.canonincal.com/v1/resouces": [
        {"code": 403, "response": "unauthorized to access endpoint"}
      ]
    }

--- uaclient/tests/test_serviceclient.py
+++ uaclient/tests/test_serviceclient.py
@@ -2,6 +2,7 @@ import mock
 from urllib.error import HTTPError, URLError
 from urllib.parse import urlencode
 from io import BytesIO
+import json
 
 import pytest
 
@@ -22,7 +23,7 @@ class OurServiceClient(UAServiceClient):
 
     @property
     def cfg_url_base_attr(self):
-        return "url_attr"
+        return "contract_url"
 
 
 class TestRequestUrl:
@@ -42,11 +43,12 @@ class TestRequestUrl:
     )
     @mock.patch("uaclient.serviceclient.util.readurl")
     def test_httperror_handling(
-        self, m_readurl, fp, expected_exception, expected_attrs
+        self, m_readurl, fp, expected_exception, expected_attrs, FakeConfig
     ):
         m_readurl.side_effect = HTTPError(None, 619, None, None, fp)
-
-        client = OurServiceClient(cfg=mock.Mock(url_attr="http://example.com"))
+        cfg = FakeConfig()
+        cfg.cfg["contract_url"] = "http://example.com"
+        client = OurServiceClient(cfg=cfg)
         with pytest.raises(expected_exception) as excinfo:
             client.request_url("/")
 
@@ -54,10 +56,12 @@ class TestRequestUrl:
             assert expected_value == getattr(excinfo.value, attr)
 
     @mock.patch("uaclient.serviceclient.util.readurl")
-    def test_urlerror_handling(self, m_readurl):
+    def test_urlerror_handling(self, m_readurl, FakeConfig):
         m_readurl.side_effect = URLError(None)
 
-        client = OurServiceClient(cfg=mock.Mock(url_attr="http://example.com"))
+        cfg = FakeConfig()
+        cfg.cfg["contract_url"] = "http://example.com"
+        client = OurServiceClient(cfg=cfg)
         with pytest.raises(util.UrlError) as excinfo:
             client.request_url("/")
 
@@ -67,13 +71,16 @@ class TestRequestUrl:
         "m_kwargs", ({"a": 1, "b": "2", "c": "try me"}, {})
     )
     @mock.patch("uaclient.serviceclient.util.readurl")
-    def test_url_query_params_append_querystring(self, m_readurl, m_kwargs):
-
+    def test_url_query_params_append_querystring(
+        self, m_readurl, m_kwargs, FakeConfig
+    ):
         m_readurl.return_value = (m_kwargs, {})  # (response, resp_headers)
 
-        client = OurServiceClient(cfg=mock.Mock(url_attr="http://example.com"))
-        assert (m_kwargs, {}) == client.request_url("/", query_params=m_kwargs)
+        cfg = FakeConfig()
         url = "http://example.com/"
+        cfg.cfg["contract_url"] = url
+        client = OurServiceClient(cfg=cfg)
+        assert (m_kwargs, {}) == client.request_url("/", query_params=m_kwargs)
         if m_kwargs:
             url += "?" + urlencode(m_kwargs)
         assert [
@@ -85,3 +92,94 @@ class TestRequestUrl:
                 timeout=None,
             )
         ] == m_readurl.call_args_list
+
+
+URL_FAKES = {
+    "http://a": [{"code": 200, "response": {"key": "val", "key2": "val2"}}],
+    "http://anerror": [{"code": 404, "response": "nothing to see"}],
+    "http://multiresp": [
+        {"code": 200, "response": "resp1"},
+        {
+            "code": 200,
+            "response": "resp2",
+            "headers": {"content-type": "application/json"},
+        },
+    ],
+}
+
+
+class Test_GetResponseOverlay:
+    @pytest.mark.parametrize(
+        "url,expected",
+        (
+            ("http://a", URL_FAKES["http://a"]),
+            ("http://multiresp", URL_FAKES["http://multiresp"]),
+        ),
+    )
+    def test_overlay_returns_matching_url(
+        self, url, expected, FakeConfig, tmpdir
+    ):
+        overlay_path = tmpdir.join("overlay.json")
+        overlay_path.write(json.dumps(URL_FAKES))
+        cfg = FakeConfig()
+        cfg.override_features(
+            {"serviceclient_url_responses": overlay_path.strpath}
+        )
+        client = OurServiceClient(cfg=cfg)
+        assert expected == client._get_response_overlay(url)
+        # url overrides are cached and not re-read from disk
+        overlay_path.write(json.dumps({}))
+        assert expected == client._get_response_overlay(url)
+
+
+class Test_GetFakeResponses:
+    @pytest.mark.parametrize(
+        "url,overlay,responses",
+        (
+            # When URL has no fakes
+            ("http://a", {}, [(None, {})]),
+            # When URL has 1 fake, repeat that response for all calls
+            (
+                "http://a",
+                URL_FAKES,
+                [(URL_FAKES["http://a"][0]["response"], {})] * 2,
+            ),
+            # When URL has >1 fake, pop through that list and repeat last item
+            (
+                "http://multiresp",
+                URL_FAKES,
+                [(URL_FAKES["http://multiresp"][0]["response"], {})]
+                + [
+                    (
+                        URL_FAKES["http://multiresp"][1]["response"],
+                        URL_FAKES["http://multiresp"][1]["headers"],
+                    )
+                ]
+                * 2,
+            ),
+            # When URL fake is code != 200 raise URLError
+            (
+                "http://anerror",
+                URL_FAKES,
+                [(URLError(URL_FAKES["http://anerror"][0]["response"]), {})],
+            ),
+        ),
+    )
+    def test_overlay_returns_matching_url(
+        self, url, overlay, responses, FakeConfig, tmpdir
+    ):
+        overlay_path = tmpdir.join("overlay.json")
+        overlay_path.write(json.dumps(overlay))
+        cfg = FakeConfig()
+        cfg.override_features(
+            {"serviceclient_url_responses": overlay_path.strpath}
+        )
+        client = OurServiceClient(cfg=cfg)
+        for response, headers in responses:
+            if isinstance(response, Exception):
+                with pytest.raises(util.UrlError) as excinfo:
+                    client._get_fake_responses(url)
+                assert 404 == excinfo.value.code
+                assert "nothing to see" == str(excinfo.value)
+            else:
+                assert (response, headers) == client._get_fake_responses(url)
