00848198aa3813553e1f7f5e5262bc41be09e446
Chad Smith <chad.smith@canonical.com>
2021-08-05 09:54:25 -0600

    testing: add overlay for any serviceclient API response
    To enable development testing of features that are not yet
    available on the contract server or security API endpoints,
    We can now provide API response overrides from a file on disk for any
    matched URL.
    
    uaclient.conf can add
    features:
      serviceclient_url_responses: <some_json_file>
    
    The represented some_json_file will contain a JSON dict keyed
    by full URL path.
    
    Each value for the URL key will be a list containing response dicts
    with a HTTP "code" key and a "response" key.
    Any servicelicent URL requests to URLs matching the
    serviceclient_url_responses will be return the faked responses
    
    Here are a few examples:
    - Fake available resources URL
    {
      "https://contracts.canonincal.com/v1/resouces": [
        {"code": 200, "response": {"resources": ["livepatch", "esm-apps"]}}
      ]
    }
    
    -Fake URLerror from available resources endpoint
    {
      "https://contracts.canonincal.com/v1/resouces": [
        {"code": 403, "response": "unauthorized to access endpoint"}
      ]
    }

--- uaclient/serviceclient.py
+++ uaclient/serviceclient.py
@@ -1,5 +1,6 @@
 import abc
 import json
+import os
 
 from urllib import error
 from urllib.parse import urlencode
@@ -9,16 +10,15 @@ from uaclient import config
 from uaclient import util
 from uaclient import version
 
-try:
-    from typing import Optional, Type  # noqa
-except ImportError:
-    # typing isn't available on trusty, so ignore its absence
-    pass
+from typing import Any, Dict, Optional, Tuple, Type  # noqa
 
 
 class UAServiceClient(metaclass=abc.ABCMeta):
 
     url_timeout = None  # type: Optional[int]
+    # Cached serviceclient_url_responses if provided in uaclient.conf
+    # via features: {serviceclient_url_responses: /some/file.json}
+    _response_overlay = None  # type: Dict[str, Any]
 
     @property
     @abc.abstractmethod
@@ -54,6 +54,9 @@ class UAServiceClient(metaclass=abc.ABCMeta):
         if headers.get("content-type") == "application/json" and data:
             data = json.dumps(data).encode("utf-8")
         url = urljoin(getattr(self.cfg, self.cfg_url_base_attr), path)
+        fake_response, fake_headers = self._get_fake_responses(url)
+        if fake_response:
+            return fake_response, fake_headers  # URL faked by uaclient.conf
         if query_params:
             # filter out None values
             filtered_params = {
@@ -83,3 +86,69 @@ class UAServiceClient(metaclass=abc.ABCMeta):
                 e, code=getattr(e, "code", None), headers=headers, url=url
             )
         return response, headers
+
+    def _get_response_overlay(self, url: str):
+        """Return a list of fake response dicts for a given URL.
+
+        serviceclient_url_responses in uaclient.conf should be a path
+        to a json file which contains a dictionary keyed by full URL path.
+        Each value will be a list of dicts representing each faked response
+        for the given URL.
+
+            The response dict item will have a code: <HTTP_STATUS_CODE> and
+               response: "some string of content".
+            The JSON string below fakes the available_resources URL on the
+            contract server:
+            '{"https://contracts.canonical.com/v1/resources": \
+               [{"code": 200, "response": {"key": "val1", "key2": "val2"}}]}'
+
+        :return: List of dicts for each faked response matching the url, or
+           and empty list when no matching url found.
+        """
+        if self._response_overlay is not None:
+            # Cache it so we don't re-read config every readurl call
+            return self._response_overlay.get(url, [])
+        response_overlay_path = self.cfg.features.get(
+            "serviceclient_url_responses"
+        )
+        if not response_overlay_path:
+            self._response_overlay = {}
+        elif not os.path.exists(response_overlay_path):
+            self._response_overlay = {}
+        else:
+            self._response_overlay = json.loads(
+                util.load_file(response_overlay_path)
+            )
+        return self._response_overlay.get(url, [])
+
+    def _get_fake_responses(
+        self, url: str
+    ) -> "Tuple[Optional[Dict[str, Any]], Optional[Dict[str,str]]]":
+        """Return response and headers if faked for this URL in uaclient.conf.
+
+        :return: A tuple of response and header dicts if the URL has an overlay
+            response defined. Return (None, {}) otherwise.
+
+        :raises util.URLError: When faked response "code" is != 200.
+            URLError reason will be "response" value and any optional
+            "headers" provided.
+        """
+        responses = self._get_response_overlay(url)
+        if not responses:
+            return None, {}
+        if len(responses) == 1:
+            # When only one respose is defined, repeat it for all calls
+            response = responses[0]
+        else:
+            # When multiple responses defined pop the first one off the list.
+            response = responses.pop(0)
+        if response["code"] == 200:
+            return response["response"], response.get("headers", {})
+        # Must be an error
+        e = error.URLError(response["response"])
+        raise util.UrlError(
+            e,
+            code=response["code"],
+            headers=response.get("headers", {}),
+            url=url,
+        )
