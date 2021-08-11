161d5469d061661048667679d80e9fd3b03de131
Renan Rodrigo <renanrodrigo@canonical.com>
2021-08-11 15:52:32 -0300

    timer: allow jobs run_interval to be configurable
    Create the TimedJob class, making sure:
    - Jobs can be extended/modified as needed and
    - Config is used when deciding the run interval
    
    Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

--- lib/timer.py
+++ lib/timer.py
@@ -1,13 +1,12 @@
 """
-Timer script used to run all jobs that need to be frequently run on the system
+Timer used to run all jobs that need to be frequently run on the system
 """
 
 import logging
 
 from datetime import datetime, timedelta
 
-import collections
-from typing import Any, Callable, Dict  # noqa: F401
+from typing import Callable
 
 from uaclient.cli import setup_logging
 from uaclient.config import UAConfig
@@ -21,79 +20,80 @@ UPDATE_MESSAGING_INTERVAL = 21600  # 6 hours
 UPDATE_STATUS_INTERVAL = 43200  # 12 hours
 GCP_AUTO_ATTACH_INTERVAL = 1800  # 30 minutes
 
-# Store the default run_interval_seconds for each job in this dict.
-# OrderedDict is used to ensure each job is run sequentially in the order
-# listed as some jobs may depend on completion of the previous job.
-UACLIENT_JOBS = collections.OrderedDict(
-    [
-        (
-            "update_messaging",
-            {
-                "job_func": update_apt_and_motd_messages,
-                "run_interval_seconds": UPDATE_MESSAGING_INTERVAL,
-            },
-        ),
-        (
-            "update_status",
-            {
-                "job_func": update_status,
-                "run_interval_seconds": UPDATE_STATUS_INTERVAL,
-            },
-        ),
-        (
-            "gcp_auto_attach",
-            {
-                "job_func": gcp_auto_attach,
-                "run_interval_seconds": GCP_AUTO_ATTACH_INTERVAL,
-            },
-        ),
-    ]
-)  # type: Dict[str, Dict[str, Any]]
+
+class TimedJob:
+    def __init__(
+        self,
+        name: str,
+        job_func: Callable[[UAConfig], None],
+        default_interval_seconds: int,
+    ):
+        self.name = name
+        self._job_func = job_func
+        self._default_interval_seconds = default_interval_seconds
+
+    def run(self, cfg: UAConfig):
+        """Run a job in a failsafe manner, returning True on success.
+
+        :param cfg: UAConfig instance
+
+        :return: A bool True when successfully run. False if ignored
+            or in error.
+        """
+        LOG.debug("Running job: %s", self.name)
+        try:
+            self._job_func(cfg)
+        except Exception as e:
+            LOG.warning("Error executing job %s: %s", self.name, str(e))
+            return False
+
+        return True
+
+    def run_interval_seconds(self, cfg: UAConfig):
+        """Return the run_interval for the job based on config or defaults."""
+        configured_interval = getattr(cfg, "{}_timer".format(self.name), None)
+        if not isinstance(configured_interval, int) or configured_interval < 0:
+            error_msg = (
+                "Invalid value for {} interval found in config. "
+                "Default value will be used."
+            ).format(self.name)
+            LOG.error(error_msg)
+            return self._default_interval_seconds
+        return configured_interval
 
 
-def run_job(
-    job_name: str, job_func: "Callable[[UAConfig], None]", cfg: UAConfig
-) -> bool:
-    """Run a job in a failsafe manner, returning True on success.
-
-    :param job_name: Name of the job to run
-    :param job_func: Job callable to call
-    :param cfg: UAConfig instance
-
-    :return: A bool True when successfully run. False if ignored or in error.
-    """
-    LOG.debug("Running job: %s", job_name)
-    try:
-        job_func(cfg)
-    except Exception as e:
-        LOG.warning("Error executing job %s: %s", job_name, str(e))
-        return False
-
-    return True
+UACLIENT_JOBS = [
+    TimedJob(
+        "update_messaging",
+        update_apt_and_motd_messages,
+        UPDATE_MESSAGING_INTERVAL,
+    ),
+    TimedJob("update_status", update_status, UPDATE_STATUS_INTERVAL),
+    TimedJob("gcp_auto_attach", gcp_auto_attach, GCP_AUTO_ATTACH_INTERVAL),
+]
 
 
 def run_jobs(cfg: UAConfig, current_time: datetime):
-    """Run ordered UACLIENT_JOBS when next_run is before utcnow.
+    """Run jobs in order when next_run is before current_time.
 
-    Persist jobs-status with calculated next_run values to aid in timer state
-    introspection for jobs which ave not yet run.
+    Persist jobs-status with calculated next_run values to aid in timer
+    state introspection for jobs which have not yet run.
     """
     LOG.debug("Trigger UA Timer jobs")
     jobs_status = cfg.read_cache("jobs-status") or {}
-    for job_name, job_info in UACLIENT_JOBS.items():
-        if job_name in jobs_status:
+    for job in UACLIENT_JOBS:
+        if job.name in jobs_status:
             next_run = datetime.strptime(
-                jobs_status[job_name]["next_run"], "%Y-%m-%dT%H:%M:%S.%f"
+                jobs_status[job.name]["next_run"], "%Y-%m-%dT%H:%M:%S.%f"
             )
             if next_run > current_time:
                 continue  # Skip job as expected next_run hasn't yet passed
-        job_func = job_info["job_func"]  # type: Callable[[UAConfig], None]
-        if run_job(job_name=job_name, job_func=job_func, cfg=cfg):
+        if job.run(cfg):
             # Persist last_run and next_run UTC-based times on job success.
-            jobs_status[job_name] = {
+            jobs_status[job.name] = {
                 "last_run": current_time,
                 "next_run": current_time
-                + timedelta(seconds=job_info["run_interval_seconds"]),
+                + timedelta(seconds=job.run_interval_seconds(cfg)),
             }
     cfg.write_cache(key="jobs-status", content=jobs_status)
 
