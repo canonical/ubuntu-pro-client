0215a79598e20a663863d754a58a6c8888c9ced3
Lucas Moura <lucas.moura@canonical.com>
2021-08-16 11:05:49 -0300

    jobs: add gcp auto-attach job
    Add gcp job that tries to run an auto-attach
    operation every 30 minutes. Before we try to run
    that command we verify that we are running on
    a GCP cloud and that we are not already attached.
    If we detect any of those scenarios, the job will
    not execute

---
+++ uaclient/jobs/gcp_auto_attach.py
@@ -0,0 +1,36 @@
+"""
+Try to auto-attach in a GCP instance. This should only work
+if the instance has a new UA license attached to it
+"""
+
+from uaclient import config
+from uaclient import exceptions
+
+from uaclient.clouds.identity import get_cloud_type
+from uaclient.cli import action_auto_attach
+
+
+def gcp_auto_attach(cfg: config.UAConfig) -> None:
+    # If the instance is already attached we will not do anything.
+    # This implies that the user may have a new license attached to the
+    # instance, but we will not perfom the change through this job.
+    if cfg.is_attached:
+        return
+
+    # We will not do anything in a non-GCP cloud
+    cloud_id, _ = get_cloud_type()
+    if not cloud_id or cloud_id != "gce":
+        return
+
+    try:
+        # This function already uses the assert lock decorator,
+        # which means that we don't need to make create another
+        # lock only for the job
+        action_auto_attach(args=None, cfg=cfg)
+    except exceptions.NonAutoAttachImageError:
+        # If we get a NonAutoAttachImageError we now
+        # that the machine is not ready yet to perform an
+        # auto-attach operation (i.e. the license may not
+        # have been appended yet). If that happens, we will not
+        # error out.
+        return
