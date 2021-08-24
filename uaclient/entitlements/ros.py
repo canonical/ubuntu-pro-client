efe6108e6b08dcbb5e61cf38e5cb853e592891e5
Lucas Moura <lucas.moura@canonical.com>
2021-08-24 15:14:46 -0300

    ros: update service related messages

--- uaclient/entitlements/ros.py
+++ uaclient/entitlements/ros.py
@@ -4,10 +4,10 @@ from uaclient.entitlements import repo
 
 
 class ROSESMEntitlement(repo.RepoEntitlement):
-    help_doc_url = ""
+    help_doc_url = "https://ubuntu.com/robotics/ros-esm"
     name = "esm-ros"
     title = "ROS ESM"
-    description = "ROS ESM Service"
+    description = "ROS Extended Security Maintenance (ESM)"
     repo_key_file = "ubuntu-advantage-esm-ros.gpg"
     is_beta = True
     _required_services = ("esm-infra", "esm-apps")  # type: Tuple[str, ...]
