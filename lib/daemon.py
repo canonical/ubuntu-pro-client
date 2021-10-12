f23ca95450ea553b64d645eb5bbaa199e621dc0f
Grant Orndorff <grant.orndorff@canonical.com>
2021-10-12 12:19:01 -0400

    wip http via threadpool

--- lib/daemon.py
+++ lib/daemon.py
@@ -1,20 +1,23 @@
+import asyncio
+import concurrent.futures
 import datetime
 import logging
 import sys
-import threading
 
 from uaclient import util
 from uaclient.cli import setup_logging
 from uaclient.clouds.identity import get_cloud_type
 
+read_url = "this is a hack"
 
-def gcp_auto_attach():
+
+async def gcp_auto_attach():
     # just a for loop in case I mess up, it'll only do whatever mistake 100 times
     for _i in range(100):
         now = datetime.datetime.now()
         logging.info("Requesting metadata at {}".format(now))
         try:
-            result = util.readurl(
+            result = await readurl(
                 "http://metadata.google.internal/computeMetadata/v1/instance/attributes/?recursive=true&wait_for_change=true",
                 headers={"Metadata-Flavor": "Google"},
             )
@@ -23,22 +26,24 @@ def gcp_auto_attach():
             logging.exception(e)
 
 
-def main():
-    cloud, _none_reason = get_cloud_type()
-    threads = []
-    if cloud == "gce":
-        thread = threading.Thread(target=gcp_auto_attach)
-        thread.start()
-        threads.append(thread)
+async def main():
+    with concurrent.futures.ThreadPoolExecutor(max_workers=20) as executor:
 
-    # maybe do other stuff in other threads one day
+        async def async_readurl(*args):
+            loop = asyncio.get_event_loop()
+            loop.run_in_executor(executor, util.readurl, *args)
 
-    for thread in threads:
-        thread.join()
+        read_url = async_readurl
 
-    return 0
+        cloud, _none_reason = get_cloud_type()
+        coroutines = []
+        if cloud == "gce":
+            coroutines.append(gcp_auto_attach())
+
+        await asyncio.gather(*coroutines)
 
 
 if __name__ == "__main__":
     setup_logging(logging.DEBUG, logging.DEBUG)
-    sys.exit(main())
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(main())
