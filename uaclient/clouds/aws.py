30dc8d91e7da591f2cf75fcd4e8b98f26490db3d
Lucas Moura <lucas.moura@canonical.com>
2021-10-22 10:38:07 -0300

    aws: support using IPv6 metadata endpoint
    We are now adding support for AWS instances to query the
    IPv6 metadata endpoint. However, we are defaulting to use
    the IPv4 endpoint first. Only if that fails, we will try
    to query the IPv6 endpoint.

--- uaclient/clouds/aws.py
+++ uaclient/clouds/aws.py
@@ -1,11 +1,17 @@
+import logging
 from typing import Any, Dict
 from urllib.error import HTTPError
 
-from uaclient import util
+from uaclient import exceptions, util
 from uaclient.clouds import AutoAttachCloudInstance
 
-IMDS_V2_TOKEN_URL = "http://169.254.169.254/latest/api/token"
-IMDS_URL = "http://169.254.169.254/latest/dynamic/instance-identity/pkcs7"
+IMDS_IPV4_ADDRESS = "169.254.169.254"
+IMDS_IPV6_ADDRESS = "[fd00:ec2::254]"
+
+IMDS_IP_ADDRESS = (IMDS_IPV4_ADDRESS, IMDS_IPV6_ADDRESS)
+IMDS_V2_TOKEN_URL = "http://{}/latest/api/token"
+IMDS_URL = "http://{}/latest/dynamic/instance-identity/pkcs7"
+
 SYS_HYPERVISOR_PRODUCT_UUID = "/sys/hypervisor/uuid"
 DMI_PRODUCT_SERIAL = "/sys/class/dmi/id/product_serial"
 DMI_PRODUCT_UUID = "/sys/class/dmi/id/product_uuid"
@@ -18,27 +24,58 @@ AWS_TOKEN_REQ_HEADER = AWS_TOKEN_PUT_HEADER + "-ttl-seconds"
 class UAAutoAttachAWSInstance(AutoAttachCloudInstance):
 
     _api_token = None
+    _ip_address = None
+
+    def _get_imds_url_response(self):
+        headers = self._request_imds_v2_token_headers()
+        return util.readurl(
+            IMDS_URL.format(self._ip_address), headers=headers, timeout=1
+        )
 
     # mypy does not handle @property around inner decorators
     # https://github.com/python/mypy/issues/1362
     @property  # type: ignore
     @util.retry(HTTPError, retry_sleeps=[1, 2, 5])
     def identity_doc(self) -> Dict[str, Any]:
-        headers = self._get_imds_v2_token_headers()
-        response, _headers = util.readurl(IMDS_URL, headers=headers)
+        response, _headers = self._get_imds_url_response()
         return {"pkcs7": response}
 
+    def _request_imds_v2_token_headers(self):
+        for address in IMDS_IP_ADDRESS:
+            try:
+                headers = self._get_imds_v2_token_headers(ip_address=address)
+            except HTTPError as e:
+                raise e
+            except Exception as e:
+                msg = (
+                    "Could not reach AWS IMDS at http://{endpoint}:"
+                    " {reason}\n".format(
+                        endpoint=address, reason=getattr(e, "reason", "")
+                    )
+                )
+                logging.debug(msg)
+            else:
+                self._ip_address = address
+                break
+        if self._ip_address is None:
+            raise exceptions.UserFacingError(
+                "No valid AWS IMDS endpoint discovered at addresses: %s"
+                % ", ".join(IMDS_IP_ADDRESS)
+            )
+        return headers
+
     @util.retry(HTTPError, retry_sleeps=[1, 2, 5])
-    def _get_imds_v2_token_headers(self):
+    def _get_imds_v2_token_headers(self, ip_address):
         if self._api_token == "IMDSv1":
             return None
         elif self._api_token:
             return {AWS_TOKEN_PUT_HEADER: self._api_token}
         try:
             response, _headers = util.readurl(
-                IMDS_V2_TOKEN_URL,
+                IMDS_V2_TOKEN_URL.format(ip_address),
                 method="PUT",
                 headers={AWS_TOKEN_REQ_HEADER: AWS_TOKEN_TTL_SECONDS},
+                timeout=1,
             )
         except HTTPError as e:
             if e.code == 404:
@@ -46,6 +83,7 @@ class UAAutoAttachAWSInstance(AutoAttachCloudInstance):
                 return None
             else:
                 raise
+
         self._api_token = response
         return {AWS_TOKEN_PUT_HEADER: self._api_token}
 
