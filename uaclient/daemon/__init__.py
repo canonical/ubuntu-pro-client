a3ddb17513b92b060e8037c0b4628fbc3eeffdc7
Grant Orndorff <grant.orndorff@canonical.com>
2021-11-22 16:10:50 -0500

    daemon: add gcp auto attach job to daemon

--- uaclient/daemon/__init__.py
+++ uaclient/daemon/__init__.py
@@ -3,17 +3,28 @@ import threading
 from typing import List
 
 from uaclient import config
+from uaclient.daemon import license_check
 
 LOG = logging.getLogger("ua.daemon")
 
 
 def on_start(cfg: config.UAConfig) -> None:
     LOG.debug("daemon.on_start called")
+    license_check.check_license(cfg)
 
 
 def start_background_threads(cfg: config.UAConfig) -> List[threading.Thread]:
     LOG.debug("daemon.start_background_threads called")
     threads = []  # type: List[threading.Thread]
+
+    if license_check.should_poll(cfg):
+        fn = license_check.get_polling_fn(cfg)
+        if fn:
+            LOG.debug("starting license check polling thread")
+            thread = threading.Thread(target=fn)
+            thread.start()
+            threads.append(thread)
+
     return threads
 
 
