016b3e9a63c17856708e5a0b00c6f0ec83e42928
Grant Orndorff <grant.orndorff@canonical.com>
2021-12-15 14:22:12 -0500

    tests: small niceties when building/testing interactively

--- tools/test-in-lxd.sh
+++ tools/test-in-lxd.sh
@@ -12,5 +12,17 @@ lxc delete $name --force
 lxc launch ubuntu-daily:$series $name
 sleep 5
 lxc file push $deb $name/tmp/ua.deb
+
+if [ -n "$SHELL_BEFORE" ]; then
+    set +x
+    echo
+    echo
+    echo "New version of ua has not been installed yet."
+    echo "After you exit the shell we'll upgrade ua and bring you right back."
+    echo
+    set -x
+    lxc exec $name bash
+fi
+
 lxc exec $name -- dpkg -i /tmp/ua.deb
 lxc exec $name bash
