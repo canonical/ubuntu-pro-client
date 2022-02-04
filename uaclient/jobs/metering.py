1d22940cf4294e35298a0ef4ba0bfa419913cb88
Teodor Petrican <teodor.petrican@gmail.com>
2022-02-04 12:45:03 +0200

    contract: send activity info during machine token update operations
    Updates the machine_token_update request to also include activity information if available, in a similar fashion to how the report_machine_activity request is behaving.

--- uaclient/jobs/metering.py
+++ uaclient/jobs/metering.py
@@ -5,8 +5,6 @@ Functions to be used when running metering jobs
 from uaclient import config
 from uaclient.cli import assert_lock_file
 from uaclient.contract import UAContractClient
-from uaclient.entitlements import ENTITLEMENT_CLASSES
-from uaclient.status import UserFacingStatus
 
 
 @assert_lock_file("timer metering job")
@@ -18,12 +16,7 @@ def metering_enabled_resources(cfg: config.UAConfig) -> bool:
     if not cfg.is_attached:
         return False
 
-    enabled_services = [
-        ent(cfg).name
-        for ent in ENTITLEMENT_CLASSES
-        if ent(cfg).user_facing_status()[0] == UserFacingStatus.ACTIVE
-    ]
-
     contract = UAContractClient(cfg)
-    contract.report_machine_activity(enabled_services=enabled_services)
+    contract.report_machine_activity()
+
     return True
