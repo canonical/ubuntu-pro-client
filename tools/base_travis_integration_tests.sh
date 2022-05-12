f2db25b65696df86435734040632cd76efdb7fe7
calvinmwadime <calvin.mwadime@canonical.com>
2022-05-12 11:28:04 +0300

    shell-linting: lint some files

--- tools/base_travis_integration_tests.sh
+++ tools/base_travis_integration_tests.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 set -e
 
-UA_STAGING_PPA=ppa:ua-client/staging
-UA_STAGING_PPA_KEYID=6E34E7116C0BC933
+export UA_STAGING_PPA=ppa:ua-client/staging
+export UA_STAGING_PPA_KEYID=6E34E7116C0BC933
 
 remove_lxd() {
   sudo apt-get remove --yes --purge lxd lxd-client
