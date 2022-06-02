0582cb156bced75fe1e26cfe9aa9ab3905c48c0e
Lucas Moura <lucas.moura@canonical.com>
2022-06-02 15:59:41 -0300

    api: add endpoints for magic-attach
    Add support for initiate, wait and delete endpoints for
    magic-attach process, where the user attaches by providing
    the email.

---
+++ uaclient/api/u/pro/attach/magic/wait/v1.py
@@ -0,0 +1,70 @@
+import time
+
+from uaclient import exceptions
+from uaclient.config import UAConfig
+from uaclient.contract import UAContractClient
+from uaclient.data_types import DataObject, Field, StringDataValue
+
+
+class MagicAttachWaitResult(DataObject):
+    fields = [
+        Field("_schema", StringDataValue),
+        Field("confirmation_code", StringDataValue),
+        Field("token", StringDataValue),
+        Field("expires", StringDataValue),
+        Field("user_email", StringDataValue),
+        Field("contract_id", StringDataValue),
+        Field("contract_token", StringDataValue),
+    ]
+
+    def __init__(
+        self,
+        _schema: str,
+        confirmation_code: str,
+        token: str,
+        expires: str,
+        user_email: str,
+        contract_id: str,
+        contract_token: str,
+    ):
+        self._schema = _schema
+        self.confirmation_code = confirmation_code
+        self.token = token
+        self.expires = expires
+        self.userEmail = user_email
+        self.contract_id = contract_id
+        self.contract_token = contract_token
+
+
+def wait(
+    magic_token: str,
+    wait_delay: int = 10,
+    cfg: UAConfig = None,
+) -> MagicAttachWaitResult:
+    if cfg is None:
+        cfg = UAConfig()
+
+    contract = UAContractClient(cfg)
+
+    while True:
+        try:
+            wait_resp = contract.get_magic_attach_token_info(
+                magic_token=magic_token
+            )
+        except exceptions.MagicAttachTokenExpired:
+            break
+
+        if wait_resp.get("contractToken") is not None:
+            return MagicAttachWaitResult(
+                _schema="0.1",
+                confirmation_code=wait_resp["confirmationCode"],
+                token=wait_resp["token"],
+                expires=wait_resp["expires"],
+                user_email=wait_resp["userEmail"],
+                contract_id=wait_resp["contractID"],
+                contract_token=wait_resp["contractToken"],
+            )
+
+        time.sleep(wait_delay)
+
+    raise exceptions.MagicAttachTokenExpired(magic_token=magic_token)
