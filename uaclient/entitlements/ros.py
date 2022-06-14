834758f3117fc86dbda93c11d7f7a48f53ace5f4
Lucas Moura <lucas.moura@canonical.com>
2022-06-14 17:16:32 -0300

    use classes for dependent/required services
    Currently, we are using the service name to stablish required or
    dependent services, but this is a shallow dependency. Since we
    already dependt directly on the service classes for incompatible
    services, we are now updating both required and dependent services
    to use that logic too.

--- uaclient/entitlements/ros.py
+++ uaclient/entitlements/ros.py
@@ -1,6 +1,7 @@
-from typing import Tuple  # noqa: F401
+from typing import Tuple, Type  # noqa: F401
 
 from uaclient.entitlements import repo
+from uaclient.entitlements.base import UAEntitlement
 
 
 class ROSCommonEntitlement(repo.RepoEntitlement):
@@ -13,16 +14,38 @@ class ROSEntitlement(ROSCommonEntitlement):
     name = "ros"
     title = "ROS ESM Security Updates"
     description = "Security Updates for the Robot Operating System"
-    _required_services = ("esm-infra", "esm-apps")  # type: Tuple[str, ...]
-    _dependent_services = ("ros-updates",)  # type: Tuple[str, ...]
+
+    @property
+    def required_services(self) -> Tuple[Type[UAEntitlement], ...]:
+        from uaclient.entitlements.esm import (
+            ESMAppsEntitlement,
+            ESMInfraEntitlement,
+        )
+
+        return (
+            ESMInfraEntitlement,
+            ESMAppsEntitlement,
+        )
+
+    @property
+    def dependent_services(self) -> Tuple[Type[UAEntitlement], ...]:
+        return (ROSUpdatesEntitlement,)
 
 
 class ROSUpdatesEntitlement(ROSCommonEntitlement):
     name = "ros-updates"
     title = "ROS ESM All Updates"
     description = "All Updates for the Robot Operating System"
-    _required_services = (
-        "esm-infra",
-        "esm-apps",
-        "ros",
-    )  # type: Tuple[str, ...]
+
+    @property
+    def required_services(self) -> Tuple[Type[UAEntitlement], ...]:
+        from uaclient.entitlements.esm import (
+            ESMAppsEntitlement,
+            ESMInfraEntitlement,
+        )
+
+        return (
+            ESMInfraEntitlement,
+            ESMAppsEntitlement,
+            ROSEntitlement,
+        )
