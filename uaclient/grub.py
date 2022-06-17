3c604efa51edd7cc6c0408a705e4e808ec4142a0
Grant Orndorff <grant.orndorff@canonical.com>
2022-06-17 16:58:26 -0400

    wip

--- uaclient/grub.py
+++ uaclient/grub.py
@@ -2,5 +2,6 @@ from uaclient import util
 
 
 def update():
-    # todo catch error and throw better error
+    # TODO catch error and throw better error
+    # TODO this might not be installed
     util.subp(["update-grub"])
