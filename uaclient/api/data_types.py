655e4b2dc96234da7db2671dc9747826eabc8230
Renan Rodrigo <renanrodrigo@canonical.com>
2022-06-24 07:39:07 -0300

    api: add data types
    Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

---
+++ uaclient/api/data_types.py
@@ -0,0 +1,64 @@
+from typing import Dict, List, Union  # noqa: F401
+
+from uaclient.data_types import DataObject, Field, StringDataValue, data_list
+from uaclient.version import get_version
+
+
+class AdditionalInfo:
+    meta = {}  # type: Dict[str, str]
+    warnings = []  # type: List[ErrorWarningObject]
+
+
+class ErrorWarningObject(DataObject):
+    fields = [
+        Field("title", StringDataValue),
+        Field("code", StringDataValue),
+        Field("meta", DataObject),
+    ]
+
+    def __init__(
+        self, *, title: str, code: str, meta: Union[DataObject, dict]
+    ):
+        self.title = title
+        self.code = code
+        self.meta = meta
+
+
+class APIData(DataObject):
+    fields = [
+        Field("type", StringDataValue),
+        Field("attributes", DataObject),
+        Field("meta", DataObject),
+    ]
+
+    def __init__(self, *, type: str, attributes: DataObject, meta: DataObject):
+        self.type = type
+        self.attributes = attributes
+        self.meta = meta
+
+
+class APIResponse(DataObject):
+    fields = [
+        Field("_schema_version", StringDataValue),
+        Field("result", StringDataValue),
+        Field("version", StringDataValue),
+        Field("errors", data_list(ErrorWarningObject)),
+        Field("warnings", data_list(ErrorWarningObject)),
+        Field("data", APIData),
+    ]
+
+    def __init__(
+        self,
+        *,
+        _schema_version: str,
+        result: str = "success",
+        errors: List[ErrorWarningObject] = [],
+        warnings: List[ErrorWarningObject] = [],
+        data: Union[APIData, dict]
+    ):
+        self._schema_version = _schema_version
+        self.result = result
+        self.version = get_version()
+        self.errors = errors
+        self.warnings = warnings
+        self.data = data
