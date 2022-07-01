77bf7c8cfef81eff0502a325efba28f0e27088af
Renan Rodrigo <renanrodrigo@canonical.com>
2022-07-01 09:13:34 -0300

    api: add API functionality
    Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

---
+++ uaclient/api/api.py
@@ -0,0 +1,120 @@
+from importlib import import_module
+from typing import Callable, List, Optional
+
+from uaclient.api.data_types import APIData, APIResponse, ErrorWarningObject
+from uaclient.api.errors import APIError, error_out
+from uaclient.data_types import DataObject, IncorrectFieldTypeError
+from uaclient.messages import (
+    API_BAD_ARGS_FORMAT,
+    API_INVALID_ENDPOINT,
+    API_MISSING_ARG,
+    API_NO_ARG_FOR_ENDPOINT,
+    API_UNKNOWN_ARG,
+)
+
+VALID_ENDPOINTS = []
+
+
+def call_api(endpoint_path: str, options: List[str]) -> APIResponse:
+    if endpoint_path not in VALID_ENDPOINTS:
+        return error_out(
+            APIError(
+                msg=API_INVALID_ENDPOINT.format(endpoint=endpoint_path).msg,
+                msg_code=API_INVALID_ENDPOINT.name,
+            )
+        )
+
+    module = import_module("uaclient.api." + endpoint_path)
+    endpoint = module.endpoint
+
+    option_warnings = []
+
+    if endpoint.options_cls:
+        kwargs = {}
+        fields = [f.key for f in endpoint.options_cls.fields]
+
+        for option in options:
+            try:
+                k, v = option.split("=")
+            except ValueError:
+                return error_out(
+                    APIError(
+                        msg=API_BAD_ARGS_FORMAT.format(arg=option).msg,
+                        msg_code=API_BAD_ARGS_FORMAT.name,
+                    )
+                )
+
+            if not k or not v:
+                return error_out(
+                    APIError(
+                        msg=API_BAD_ARGS_FORMAT.format(arg=option).msg,
+                        msg_code=API_BAD_ARGS_FORMAT.name,
+                    )
+                )
+
+            if k not in fields:
+                option_warnings.append(
+                    ErrorWarningObject(
+                        title=API_UNKNOWN_ARG.format(arg=k).msg,
+                        code=API_UNKNOWN_ARG.name,
+                        meta={},
+                    )
+                )
+
+            kwargs[k] = v
+
+        try:
+            options = endpoint.options_cls.from_dict(kwargs)
+        except IncorrectFieldTypeError as e:
+            return error_out(
+                APIError(
+                    msg=API_MISSING_ARG.format(
+                        arg=e.key, endpoint=endpoint_path
+                    ).msg,
+                    msg_code=API_MISSING_ARG.name,
+                )
+            )
+
+        try:
+            result = endpoint.fn(options)
+        except Exception as e:
+            return error_out(e)
+
+    else:
+        if options:
+            return error_out(
+                APIError(
+                    msg=API_NO_ARG_FOR_ENDPOINT.format(
+                        endpoint=endpoint_path
+                    ).msg,
+                    msg_code=API_NO_ARG_FOR_ENDPOINT.name,
+                )
+            )
+        try:
+            result = endpoint.fn()
+        except Exception as e:
+            return error_out(e)
+
+    return APIResponse(
+        _schema_version=endpoint.version,
+        warnings=result.warnings + option_warnings,
+        data=APIData(
+            type=endpoint.name,
+            attributes=result,
+            meta=result.meta,
+        ),
+    )
+
+
+class APIEndpoint:
+    def __init__(
+        self,
+        version: str,
+        name: str,
+        fn: Callable,
+        options_cls: Optional[DataObject],
+    ):
+        self.version = version
+        self.name = name
+        self.fn = fn
+        self.options_cls = options_cls
