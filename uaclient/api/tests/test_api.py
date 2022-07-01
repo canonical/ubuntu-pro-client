97ba9d41bc8a71dd1835c13486d5aa308a08e975
Lucas Moura <lucas.moura@canonical.com>
2022-07-01 16:55:31 -0300

    update magic attach to follow api structure

--- uaclient/api/tests/test_api.py
+++ uaclient/api/tests/test_api.py
@@ -57,8 +57,8 @@ class TestAPIErrors:
 
 class TestAPICall:
     @mock.patch("uaclient.api.api.error_out")
-    def test_invalid_endpoint(self, m_error_out):
-        result = call_api("invalid_endpoint", [])
+    def test_invalid_endpoint(self, m_error_out, FakeConfig):
+        result = call_api("invalid_endpoint", [], cfg=FakeConfig())
         assert result == m_error_out.return_value
         assert m_error_out.call_count == 1
         arg = m_error_out.call_args[0][0]
@@ -75,12 +75,12 @@ class TestAPICall:
     @mock.patch("uaclient.api.api.error_out")
     @mock.patch("uaclient.api.api.import_module")
     def test_bad_formatted_args(
-        self, _m_import_module, m_error_out, arguments
+        self, _m_import_module, m_error_out, arguments, FakeConfig
     ):
         with mock.patch(
             "uaclient.api.api.VALID_ENDPOINTS", ["example_endpoint"]
         ):
-            result = call_api("example_endpoint", arguments)
+            result = call_api("example_endpoint", arguments, FakeConfig)
 
         assert result == m_error_out.return_value
         assert m_error_out.call_count == 1
@@ -92,7 +92,9 @@ class TestAPICall:
     @pytest.mark.parametrize("options_cls", (None, mock.MagicMock()))
     @mock.patch("uaclient.api.api.error_out")
     @mock.patch("uaclient.api.api.import_module")
-    def test_wrong_args(self, m_import_module, m_error_out, options_cls):
+    def test_wrong_args(
+        self, m_import_module, m_error_out, options_cls, FakeConfig
+    ):
         mock_endpoint = mock.MagicMock()
         mock_endpoint.options_cls = options_cls
 
@@ -106,7 +108,7 @@ class TestAPICall:
         with mock.patch(
             "uaclient.api.api.VALID_ENDPOINTS", ["example_endpoint"]
         ):
-            result = call_api("example_endpoint", ["not=valid"])
+            result = call_api("example_endpoint", ["not=valid"], FakeConfig())
 
         assert result == m_error_out.return_value
         assert m_error_out.call_count == 1
@@ -131,7 +133,7 @@ class TestAPICall:
             assert arg.msg_code == API_NO_ARG_FOR_ENDPOINT.name
 
     @mock.patch("uaclient.api.api.import_module")
-    def test_warning_on_extra_args(self, m_import_module):
+    def test_warning_on_extra_args(self, m_import_module, FakeConfig):
         mock_endpoint = mock.MagicMock()
         mock_endpoint.fn.return_value.warnings = []
         mock_endpoint.options_cls.fields = []
@@ -141,7 +143,9 @@ class TestAPICall:
         with mock.patch(
             "uaclient.api.api.VALID_ENDPOINTS", ["example_endpoint"]
         ):
-            result = call_api("example_endpoint", ["extra=argument"])
+            result = call_api(
+                "example_endpoint", ["extra=argument"], FakeConfig()
+            )
 
         assert len(result.warnings) == 1
         warning = result.warnings[0]
@@ -154,7 +158,9 @@ class TestAPICall:
         ((None, []), (mock.MagicMock(), ["key=value"])),
     )
     @mock.patch("uaclient.api.api.import_module")
-    def test_call_endpoint(self, m_import_module, options_cls, arguments):
+    def test_call_endpoint(
+        self, m_import_module, options_cls, arguments, FakeConfig
+    ):
         mock_endpoint = mock.MagicMock()
         mock_endpoint.options_cls = options_cls
 
@@ -162,11 +168,12 @@ class TestAPICall:
         mock_endpoint.fn = mock_endpoint_fn
 
         m_import_module.return_value.endpoint = mock_endpoint
+        cfg = FakeConfig()
 
         with mock.patch(
             "uaclient.api.api.VALID_ENDPOINTS", ["example_endpoint"]
         ):
-            result = call_api("example_endpoint", arguments)
+            result = call_api("example_endpoint", arguments, cfg)
 
         assert isinstance(result, APIResponse)
         assert result.result == "success"
@@ -177,14 +184,16 @@ class TestAPICall:
                 mock.call({"key": "value"})
             ]
             assert mock_endpoint_fn.call_args_list == [
-                mock.call(options_cls.from_dict.return_value)
+                mock.call(options_cls.from_dict.return_value, cfg)
             ]
         else:
-            assert mock_endpoint_fn.call_args_list == [mock.call()]
+            assert mock_endpoint_fn.call_args_list == [mock.call(cfg)]
 
     @mock.patch("uaclient.api.api.error_out")
     @mock.patch("uaclient.api.api.import_module")
-    def test_endpoint_function_error(self, m_import_module, m_error_out):
+    def test_endpoint_function_error(
+        self, m_import_module, m_error_out, FakeConfig
+    ):
         mock_endpoint = mock.MagicMock()
         exception = UserFacingError(
             "something is wrong", "fn-specific", {"extra": "info"}
@@ -196,14 +205,14 @@ class TestAPICall:
         with mock.patch(
             "uaclient.api.api.VALID_ENDPOINTS", ["example_endpoint"]
         ):
-            result = call_api("example_endpoint", [])
+            result = call_api("example_endpoint", [], FakeConfig())
 
         assert result == m_error_out.return_value
         assert m_error_out.call_count == 1
         assert m_error_out.call_args[0][0] == exception
 
     @mock.patch("uaclient.api.api.import_module")
-    def test_endpoint_function_warning(self, m_import_module):
+    def test_endpoint_function_warning(self, m_import_module, FakeConfig):
         mock_endpoint = mock.MagicMock()
         mock_endpoint.fn.return_value.warnings = [
             ErrorWarningObject(
@@ -219,7 +228,7 @@ class TestAPICall:
         with mock.patch(
             "uaclient.api.api.VALID_ENDPOINTS", ["example_endpoint"]
         ):
-            result = call_api("example_endpoint", [])
+            result = call_api("example_endpoint", [], FakeConfig())
 
         assert len(result.warnings) == 1
         warning = result.warnings[0]
---
+++ uaclient/api/u/pro/attach/magic/revoke/v1.py
@@ -0,0 +1,33 @@
+from uaclient.api.api import APIEndpoint
+from uaclient.api.data_types import AdditionalInfo
+from uaclient.config import UAConfig
+from uaclient.contract import UAContractClient
+from uaclient.data_types import DataObject, Field, StringDataValue
+
+
+class MagicAttachRevokeOptions(DataObject):
+    fields = [
+        Field("magic_token", StringDataValue),
+    ]
+
+    def __init__(self, magic_token):
+        self.magic_token = magic_token
+
+
+class MagicAttachRevokeResult(DataObject, AdditionalInfo):
+    pass
+
+
+def revoke(options: MagicAttachRevokeOptions, cfg: UAConfig) -> DataObject:
+    contract = UAContractClient(cfg)
+    contract.revoke_magic_attach_token(options.magic_token)
+
+    return MagicAttachRevokeResult()
+
+
+endpoint = APIEndpoint(
+    version="v1",
+    name="MagicAttachRevoke",
+    fn=revoke,
+    options_cls=MagicAttachRevokeOptions,
+)
