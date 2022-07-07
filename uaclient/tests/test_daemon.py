04398116bd292ee6c87919d3a6535ae38398a528
Grant Orndorff <grant.orndorff@canonical.com>
2022-07-07 12:57:54 -0400

    daemon: add timeout to start/stop

--- uaclient/tests/test_daemon.py
+++ uaclient/tests/test_daemon.py
@@ -1,3 +1,5 @@
+from subprocess import TimeoutExpired
+
 import mock
 import pytest
 
@@ -19,16 +21,26 @@ class TestStart:
     def test_start_success(self, m_subp):
         start()
         assert [
-            mock.call(["systemctl", "start", "ubuntu-advantage.service"])
+            mock.call(
+                ["systemctl", "start", "ubuntu-advantage.service"], timeout=2.0
+            )
         ] == m_subp.call_args_list
 
+    @pytest.mark.parametrize(
+        "err",
+        (
+            exceptions.ProcessExecutionError("cmd"),
+            TimeoutExpired("cmd", 2.0),
+        ),
+    )
     @mock.patch(M_PATH + "LOG.warning")
-    def test_start_warning(self, m_log_warning, m_subp):
-        err = exceptions.ProcessExecutionError("cmd")
+    def test_start_warning(self, m_log_warning, m_subp, err):
         m_subp.side_effect = err
         start()
         assert [
-            mock.call(["systemctl", "start", "ubuntu-advantage.service"])
+            mock.call(
+                ["systemctl", "start", "ubuntu-advantage.service"], timeout=2.0
+            )
         ] == m_subp.call_args_list
         assert [mock.call(err)] == m_log_warning.call_args_list
 
@@ -38,16 +50,26 @@ class TestStop:
     def test_stop_success(self, m_subp):
         stop()
         assert [
-            mock.call(["systemctl", "stop", "ubuntu-advantage.service"])
+            mock.call(
+                ["systemctl", "stop", "ubuntu-advantage.service"], timeout=2.0
+            )
         ] == m_subp.call_args_list
 
+    @pytest.mark.parametrize(
+        "err",
+        (
+            exceptions.ProcessExecutionError("cmd"),
+            TimeoutExpired("cmd", 2.0),
+        ),
+    )
     @mock.patch(M_PATH + "LOG.warning")
-    def test_stop_warning(self, m_log_warning, m_subp):
-        err = exceptions.ProcessExecutionError("cmd")
+    def test_stop_warning(self, m_log_warning, m_subp, err):
         m_subp.side_effect = err
         stop()
         assert [
-            mock.call(["systemctl", "stop", "ubuntu-advantage.service"])
+            mock.call(
+                ["systemctl", "stop", "ubuntu-advantage.service"], timeout=2.0
+            )
         ] == m_subp.call_args_list
         assert [mock.call(err)] == m_log_warning.call_args_list
 
