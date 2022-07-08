889e9c736f083c2a90b59503621036ea60348b57
Grant Orndorff <grant.orndorff@canonical.com>
2022-07-08 10:14:58 -0400

    version: don't fall back to changelog
    now that CI will ensure the hardcoded version and changelog match, we
    no longer need this logic

--- uaclient/tests/test_version.py
+++ uaclient/tests/test_version.py
@@ -2,7 +2,6 @@ import os.path
 
 import mock
 
-from uaclient import exceptions
 from uaclient.version import get_version
 
 
@@ -28,37 +27,6 @@ class TestGetVersion:
                 ["git", "describe", "--abbrev=8", "--match=[0-9]*", "--long"]
             )
         ] == m_subp.call_args_list
-        top_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
-        top_dir_git = os.path.join(top_dir, ".git")
-        assert [mock.call(top_dir_git)] == m_exists.call_args_list
-
-    @mock.patch("uaclient.version.PACKAGED_VERSION", "@@PACKAGED_VERSION")
-    @mock.patch("uaclient.version.os.path.exists", return_value=True)
-    def test_returns_dpkg_parsechangelog_on_git_ubuntu_pkg_branch(
-        self, m_exists, m_subp
-    ):
-        """Call dpkg-parsechangelog if git describe fails to --match=[0-9]*"""
-
-        def fake_subp(cmd):
-            if cmd[0] == "git":
-                # Not matching tag on git-ubuntu pkg branches
-                raise exceptions.ProcessExecutionError(
-                    "fatal: No names found, cannot describe anything."
-                )
-            if cmd[0] == "dpkg-parsechangelog":
-                return ("24.1\n", "")
-            assert False, "Unexpected subp cmd {}".format(cmd)
-
-        m_subp.side_effect = fake_subp
-
-        assert "24.1" == get_version()
-        expected_calls = [
-            mock.call(
-                ["git", "describe", "--abbrev=8", "--match=[0-9]*", "--long"]
-            ),
-            mock.call(["dpkg-parsechangelog", "-S", "version"]),
-        ]
-        assert expected_calls == m_subp.call_args_list
         top_dir = os.path.dirname(os.path.dirname(os.path.dirname(__file__)))
         top_dir_git = os.path.join(top_dir, ".git")
         assert [mock.call(top_dir_git)] == m_exists.call_args_list
