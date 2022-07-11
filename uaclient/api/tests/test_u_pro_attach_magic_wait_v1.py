c85dc5582397345931221d7182442d5d4f5b1fde
Lucas Moura <lucas.moura@canonical.com>
2022-07-11 11:31:09 -0300

    magic-attach: Don't fail on first connection error
    When running the wait endpoint, we will now try at least 3 times
    if we receive a connectivity error. The rationale for that is
    to avoid flake connectivity errors to stop the execution of the command

--- uaclient/api/tests/test_u_pro_attach_magic_wait_v1.py
+++ uaclient/api/tests/test_u_pro_attach_magic_wait_v1.py
@@ -60,3 +60,49 @@ class TestSimplifiedAttachWaitV1:
         with pytest.raises(exceptions.MagicAttachTokenError):
             with mock.patch.object(api_wait, "MAXIMUM_ATTEMPTS", 2):
                 wait(options, FakeConfig())
+
+    @mock.patch("time.sleep")
+    def test_wait_fails_after_number_of_connectiviry_errors(
+        self, m_sleep, m_attach_token_info, FakeConfig
+    ):
+        magic_token = "test-id"
+        m_attach_token_info.side_effect = [
+            exceptions.ConnectivityError(),
+            exceptions.ConnectivityError(),
+            exceptions.ConnectivityError(),
+            exceptions.ConnectivityError(),
+        ]
+
+        options = MagicAttachWaitOptions(magic_token=magic_token)
+
+        with pytest.raises(exceptions.ConnectivityError):
+            wait(options, FakeConfig())
+
+        assert 3 == m_sleep.call_count
+
+    @mock.patch("time.sleep")
+    def test_wait_succeds_after_number_of_connectiviry_errors(
+        self, m_sleep, m_attach_token_info, FakeConfig
+    ):
+        magic_token = "test-id"
+        m_attach_token_info.side_effect = [
+            exceptions.ConnectivityError(),
+            exceptions.ConnectivityError(),
+            exceptions.ConnectivityError(),
+            {
+                "token": magic_token,
+                "expires": "2100-06-09T18:14:55.323733Z",
+                "expiresIn": "2000",
+                "userCode": "1234",
+                "contractToken": "ctoken",
+                "contractID": "cid",
+            },
+        ]
+
+        options = MagicAttachWaitOptions(magic_token=magic_token)
+        expected_response = wait(options, FakeConfig())
+
+        assert expected_response.contract_token == "ctoken"
+        assert expected_response.contract_id == "cid"
+        assert 4 == m_attach_token_info.call_count
+        assert 3 == m_sleep.call_count
