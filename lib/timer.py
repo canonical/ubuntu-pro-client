a7bd41e38599233fb6c5433cfb1261721c8a294b
Renan Rodrigo <renanrodrigo@canonical.com>
2022-09-16 13:49:40 -0300

    jobs: don't run the EOL job
    At least temporarily, until we find out if it is feasible or if there is
    a better solution.
    
    Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

--- lib/timer.py
+++ lib/timer.py
@@ -8,7 +8,8 @@ from typing import Callable
 
 from uaclient.cli import setup_logging
 from uaclient.config import UAConfig
-from uaclient.jobs.eol_status import check_eol_and_update
+
+# from uaclient.jobs.eol_status import check_eol_and_update
 from uaclient.jobs.metering import metering_enabled_resources
 from uaclient.jobs.update_messaging import update_apt_and_motd_messages
 from uaclient.jobs.update_state import update_status
@@ -103,7 +104,7 @@ UACLIENT_JOBS = [
         UPDATE_MESSAGING_INTERVAL,
     ),
     TimedJob("update_status", update_status, UPDATE_STATUS_INTERVAL),
-    TimedJob("eol_status", check_eol_and_update, EOL_STATUS_INTERVAL),
+    # TimedJob("eol_status", check_eol_and_update, EOL_STATUS_INTERVAL),
     MeteringTimedJob(metering_enabled_resources, METERING_INTERVAL),
 ]
 
