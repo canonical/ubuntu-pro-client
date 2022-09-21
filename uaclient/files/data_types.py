fc60ec96976ce68254131e4d00f29f4cbf1a662d
Lucas Moura <lucas.moura@canonical.com>
2022-09-21 17:43:57 -0300

    files: extract services-once-enabled from config
    Extract the services-once-enabled state file from the UAConfig
    class and handle it on a dedicated module

---
+++ uaclient/files/data_types.py
@@ -0,0 +1,69 @@
+import json
+from enum import Enum
+from typing import Callable, Dict, Generic, Optional, Type, TypeVar
+
+import yaml
+
+from uaclient import exceptions
+from uaclient.data_types import DataObject
+from uaclient.files.files import UAFile
+
+
+class DataObjectFileFormat(Enum):
+    JSON = "json"
+    YAML = "yaml"
+
+
+DOFType = TypeVar("DOFType", bound=DataObject)
+
+
+class DataObjectFile(Generic[DOFType]):
+    def __init__(
+        self,
+        data_object_cls: Type[DOFType],
+        ua_file: UAFile,
+        file_format: DataObjectFileFormat = DataObjectFileFormat.JSON,
+        preprocess_data: Optional[Callable[[Dict], Dict]] = None,
+    ):
+        self.data_object_cls = data_object_cls
+        self.ua_file = ua_file
+        self.file_format = file_format
+        self.preprocess_data = preprocess_data
+
+    def read(self) -> Optional[DOFType]:
+        raw_data = self.ua_file.read()
+        if raw_data is None:
+            return None
+
+        parsed_data = None
+        if self.file_format == DataObjectFileFormat.JSON:
+            try:
+                parsed_data = json.loads(raw_data)
+            except json.JSONDecodeError:
+                raise exceptions.InvalidFileFormatError(
+                    self.ua_file.path, "json"
+                )
+        elif self.file_format == DataObjectFileFormat.YAML:
+            try:
+                parsed_data = yaml.safe_load(raw_data)
+            except yaml.parser.ParserError:
+                raise exceptions.InvalidFileFormatError(
+                    self.ua_file.path, "yaml"
+                )
+
+        if parsed_data is None:
+            return None
+
+        if self.preprocess_data:
+            parsed_data = self.preprocess_data(parsed_data)
+
+        return self.data_object_cls.from_dict(parsed_data)
+
+    def write(self, content: DOFType):
+        if self.file_format == DataObjectFileFormat.JSON:
+            str_content = content.to_json()
+        elif self.file_format == DataObjectFileFormat.YAML:
+            data = content.to_dict()
+            str_content = yaml.dump(data, default_flow_style=False)
+
+        self.ua_file.write(str_content)
---
+++ uaclient/files/tests/test_files.py
@@ -0,0 +1,49 @@
+import os
+
+from uaclient import system
+from uaclient.files import MachineTokenFile, UAFile
+
+
+class TestUAFile:
+    def test_read_write(self, tmpdir):
+        file_name = "temp_file"
+        file = UAFile(file_name, tmpdir.strpath, False)
+        content = "dummy file words"
+        file.write(content)
+        path = os.path.join(tmpdir.strpath, file_name)
+        res = system.load_file(path)
+        assert res == file.read()
+        assert res == content
+
+
+class TestMachineTokenFile:
+    def test_deleting(self, tmpdir):
+        token_file = MachineTokenFile(
+            directory=tmpdir.strpath,
+        )
+        token = {"machineTokenInfo": {"machineId": "random-id"}}
+        token_file.write(token)
+        assert token_file.machine_token == token
+        token_file.delete()
+        assert token_file.machine_token is None
+
+    def test_public_file_filtering(self, tmpdir):
+        # root access of machine token file
+        token_file = MachineTokenFile(
+            directory=tmpdir.strpath,
+        )
+        token = {
+            "machineTokenInfo": {"machineId": "random-id"},
+            "machineToken": "token",
+        }
+        token_file.write(token)
+        root_token = token_file.machine_token
+        assert token == root_token
+        # non root access of machine token file
+        token_file = MachineTokenFile(
+            directory=tmpdir.strpath, root_mode=False
+        )
+        nonroot_token = token_file.machine_token
+        assert root_token != nonroot_token
+        machine_token = nonroot_token.get("machineToken", None)
+        assert machine_token is None
