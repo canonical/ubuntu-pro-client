56b6f47c26f9ca2d9de8dfbf47daebfd3210a174
Lucas Moura <lucas.moura@canonical.com>
2022-09-29 12:05:40 -0300

    Make esm-apps beta again

--- uaclient/tests/test_cli_disable.py
+++ uaclient/tests/test_cli_disable.py
@@ -39,8 +39,8 @@ Disable an Ubuntu Pro service.
 
 Arguments:
   service              the name(s) of the Ubuntu Pro services to disable. One
-                       of: cc-eal, cis, esm-apps, esm-infra, fips, fips-
-                       updates, livepatch, ros, ros-updates
+                       of: cc-eal, cis, esm-infra, fips, fips-updates,
+                       livepatch, ros, ros-updates
 
 Flags:
   -h, --help           show this help message and exit
