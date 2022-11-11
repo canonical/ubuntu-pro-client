d88b2c8d22cf9ad4da2e25b2a69877076ddf3d2f
Lucas Moura <lucas.moura@canonical.com>
2022-11-11 14:42:27 -0300

    Bump version to 27.12

--- uaclient/version.py
+++ uaclient/version.py
@@ -18,7 +18,7 @@ from uaclient.defaults import CANDIDATE_CACHE_PATH, UAC_RUN_PATH
 from uaclient.exceptions import ProcessExecutionError
 from uaclient.system import subp
 
-__VERSION__ = "27.11.3"
+__VERSION__ = "27.12"
 PACKAGED_VERSION = "@@PACKAGED_VERSION@@"
 
 CANDIDATE_REGEX = r"Candidate: (?P<candidate>.*?)\n"
