b2c08986cb9a9019e978c231d2a199703f5378ae
Renan Rodrigo <renanrodrigo@canonical.com>
2023-01-30 13:09:45 -0300

    apt: do not crash or degrade systemctl on esm-cache update failures
    LP: #2004130
    
    Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

--- features/steps/network.py
+++ features/steps/network.py
@@ -1,7 +1,30 @@
+import socket
+
 from behave import when
 
 from features.steps.shell import when_i_run_command
 from features.util import SUT
+
+
+@when("I disable access to {website}")
+def block_access_to_address(context, website):
+    address_info = socket.getaddrinfo(website, None)
+    # Don't believe the magic? The C code will return a 5-tuple where the
+    # actual socket address is the last item, and it is a 4-tuple for ipv6 or
+    # a 2-tuple for ipv4, address string being the first item.
+    addresses_to_block = set([address[-1][0] for address in address_info])
+    for address in addresses_to_block:
+        when_i_run_command(
+            context,
+            "ufw reject out from any to {}".format(address),
+            "with sudo",
+        )
+    when_i_run_command(
+        context,
+        "ufw enable",
+        "with sudo",
+        stdin="y\n",
+    )
 
 
 @when("I disable any internet connection on the machine")
