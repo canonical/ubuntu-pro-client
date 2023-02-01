60d10c8add65660809f5387bf44aead37efcd610
Lucas Moura <lucas.moura@canonical.com>
2023-02-01 10:33:39 -0300

    apt: change esm security updates message
    Change apt esm message to make it clearer that any esm service
    is providing more security updates, not restricting the user to
    only get updates if they have the service enabled

--- apt-hook/json-hook.hh
+++ apt-hook/json-hook.hh
@@ -14,6 +14,8 @@ struct security_package_counts {
     int64_t esm_apps;
 };
 
+enum ESMType {APPS, INFRA};
+
 bool read_jsonrpc_request(std::istream &in, jsonrpc_request &req);
 bool string_ends_with(std::string str, std::string ends_with);
 bool version_from_origin_and_archive_ends_with(json_object *version, std::string from_origin, std::string archive_ends_with);
