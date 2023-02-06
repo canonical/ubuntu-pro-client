154d0c154f6d97fd84aec071a342543e37790ca6
Lucas Moura <lucas.moura@canonical.com>
2023-02-06 14:44:03 -0300

    esm_cache: do not fail for unexpected exceptions
    If an exception is raised, we will now log that exception,
    but not fail the job itself

--- uaclient/tests/test_esm_cache.py
+++ uaclient/tests/test_esm_cache.py
@@ -1,11 +1,35 @@
+import logging
+
 import mock
+import pytest
 
 from lib.esm_cache import main
+from uaclient.exceptions import MissingSeriesOnOSReleaseFile
 
 
+@mock.patch("lib.esm_cache.update_esm_caches")
 class TestUpdateEsmCaches:
-    @mock.patch("lib.esm_cache.update_esm_caches")
     def test_builds_local_cache(self, m_update_caches, FakeConfig):
         main(FakeConfig())
 
         assert m_update_caches.call_count == 1
+
+    @pytest.mark.parametrize("caplog_text", [logging.ERROR], indirect=True)
+    def test_log_user_facing_exception(
+        self, m_update_caches, caplog_text, FakeConfig
+    ):
+        expected_exception = MissingSeriesOnOSReleaseFile(version="version")
+        m_update_caches.side_effect = expected_exception
+        main(cfg=FakeConfig())
+
+        for line in expected_exception.msg.split("\n"):
+            assert line in caplog_text()
+
+    @pytest.mark.parametrize("caplog_text", [logging.ERROR], indirect=True)
+    def test_log_exception(self, m_update_caches, caplog_text, FakeConfig):
+        expected_msg = "unexpected exception"
+        expected_exception = Exception(expected_msg)
+        m_update_caches.side_effect = expected_exception
+        main(cfg=FakeConfig())
+
+        assert expected_msg in caplog_text()
