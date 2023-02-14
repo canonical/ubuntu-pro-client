08107b50791a1853604a7b4fb8128def2ed5b50b
Alberto Contreras <alberto.contreras@canonical.com>
2023-02-14 10:28:59 +0100

    tools: improve test-in-{lxd,multipass}.sh
    - remove dead code
    - enable errexit and nounset flags
    - provide default for series
    - fix jq argument

--- tools/test-in-multipass.sh
+++ tools/test-in-multipass.sh
@@ -1,31 +1,19 @@
 #!/usr/bin/bash
-series=$1
+set -eux
 
-set -x
-
-build_out=$(./tools/build.sh $series)
-hash=$(echo $build_out | jq -r .state_hash)
-deb=$(echo $build_out | jq -r .debs[] | grep tools)
+series=${1:-jammy}
+build_out=$(./tools/build.sh "$series")
+hash=$(echo "$build_out" | jq -r .state_hash)
+deb=$(echo "$build_out" | jq -r '.debs[]' | grep tools)
 name=ua-$series-$hash
 
-multipass delete $name --purge
-multipass launch $series --name $name
+multipass delete "$name" --purge || true
+multipass launch "$series" --name "$name"
 sleep 30
 # Snaps won't access /tmp
-cp $deb ~/ua.deb
-multipass transfer ~/ua.deb $name:/tmp/ua.deb
+cp "$deb" ~/ua.deb
+multipass transfer ~/ua.deb "${name}:/tmp/ua.deb"
 rm -f ~/ua.deb
 
-if [ -n "$SHELL_BEFORE" ]; then
-    set +x
-    echo
-    echo
-    echo "New version of pro has not been installed yet."
-    echo "After you exit the shell we'll upgrade pro and bring you right back."
-    echo
-    set -x
-    multipass exec $name bash
-fi
-
-multipass exec $name -- sudo dpkg -i /tmp/ua.deb
-multipass shell $name
+multipass exec "$name" -- sudo dpkg -i /tmp/ua.deb
+multipass shell "$name"
