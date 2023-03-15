Monochromics/ubuntu-advantage-client

notices: refactor to have separate files

uaclient/tests/test_cli.py
@@ -31,6 +31,7 @@ from uaclient.exceptions import (
     UnattachedError,
     UserFacingError,
 )
+from uaclient.files.notices import Notice
 
 BIG_DESC = "123456789 " * 7 + "next line"
 BIG_URL = "http://" + "adsf" * 10
@@ -355,13 +356,13 @@ M_PATH_UACONFIG = "uaclient.config.UAConfig."
 class TestAssertLockFile:
     @mock.patch("os.getpid", return_value=123)
     @mock.patch(M_PATH_UACONFIG + "delete_cache_key")
-    @mock.patch("uaclient.files.NoticeFile.add")
+    @mock.patch("uaclient.files.notices.Notices.add")
     @mock.patch(M_PATH_UACONFIG + "write_cache")
     def test_assert_root_creates_lock_and_notice(
         self,
         m_write_cache,
         m_add_notice,
-        m_remove_notice,
+        m_delete_cache,
         _m_getpid,
         FakeConfig,
     ):
@@ -377,8 +378,10 @@ class TestAssertLockFile:
         ret = test_function(arg, cfg=FakeConfig())
         assert mock.sentinel.success == ret
         lock_msg = "Operation in progress: some operation"
-        assert [mock.call("", lock_msg)] == m_add_notice.call_args_list
-        assert [mock.call("lock")] == m_remove_notice.call_args_list
+        assert [
+            mock.call(True, Notice.OPERATION_IN_PROGRESS, lock_msg)
+        ] == m_add_notice.call_args_list
+        assert [mock.call("lock")] == m_delete_cache.call_args_list
         assert [
             mock.call("lock", "123:some operation")
         ] == m_write_cache.call_args_list

uaclient/tests/test_cli_attach.py
@@ -152,7 +152,12 @@ class TestActionAttach:
 
     @mock.patch("uaclient.system.subp")
     def test_lock_file_exists(
-        self, m_subp, _m_getuid, capsys, FakeConfig, event
+        self,
+        m_subp,
+        _m_getuid,
+        capsys,
+        FakeConfig,
+        event,
     ):
         """Check when an operation holds a lock file, attach cannot run."""
         cfg = FakeConfig()
@@ -197,7 +202,11 @@ class TestActionAttach:
         assert expected == json.loads(capsys.readouterr()[0])
 
     def test_token_is_a_required_argument(
-        self, _m_getuid, FakeConfig, capsys, event
+        self,
+        _m_getuid,
+        FakeConfig,
+        capsys,
+        event,
     ):
         """When missing the required token argument, raise a UserFacingError"""
         args = mock.MagicMock(token=None, attach_config=None)
@@ -246,7 +255,7 @@ class TestActionAttach:
         ),
     )
     @mock.patch("uaclient.system.should_reboot", return_value=False)
-    @mock.patch("uaclient.files.NoticeFile.remove")
+    @mock.patch("uaclient.files.notices.Notices.remove")
     @mock.patch("uaclient.status.get_available_resources")
     @mock.patch("uaclient.jobs.update_messaging.update_apt_and_motd_messages")
     @mock.patch(M_PATH + "contract.request_updated_contract")
@@ -255,8 +264,8 @@ class TestActionAttach:
         request_updated_contract,
         m_update_apt_and_motd_msgs,
         _m_get_available_resources,
-        _m_should_reboot,
         _m_remove_notice,
+        _m_should_reboot,
         _m_get_uid,
         error_class,
         error_str,
@@ -288,7 +297,7 @@ class TestActionAttach:
         assert [mock.call(cfg)] == m_update_apt_and_motd_msgs.call_args_list
 
     @mock.patch("uaclient.system.should_reboot", return_value=False)
-    @mock.patch("uaclient.files.NoticeFile.remove")
+    @mock.patch("uaclient.files.notices.Notices.remove")
     @mock.patch("uaclient.jobs.update_messaging.update_apt_and_motd_messages")
     @mock.patch(
         M_PATH + "contract.UAContractClient.request_contract_machine_attach"
@@ -301,8 +310,8 @@ class TestActionAttach:
         m_status,
         contract_machine_attach,
         m_update_apt_and_motd_msgs,
-        _m_should_reboot,
         _m_remove_notice,
+        _m_should_reboot,
         _m_getuid,
         FakeConfig,
         event,
@@ -363,15 +372,15 @@ class TestActionAttach:
 
     @pytest.mark.parametrize("auto_enable", (True, False))
     @mock.patch("uaclient.system.should_reboot", return_value=False)
-    @mock.patch("uaclient.files.NoticeFile.remove")
+    @mock.patch("uaclient.files.notices.Notices.remove")
     @mock.patch("uaclient.status.get_available_resources")
     @mock.patch("uaclient.jobs.update_messaging.update_apt_and_motd_messages")
     def test_auto_enable_passed_through_to_request_updated_contract(
         self,
         m_update_apt_and_motd_msgs,
         _m_get_available_resources,
-        _m_should_reboot,
         _m_remove_notice,
+        _m_should_reboot,
         _m_get_uid,
         auto_enable,
         FakeConfig,
@@ -392,7 +401,9 @@ class TestActionAttach:
         assert [mock.call(cfg)] == m_update_apt_and_motd_msgs.call_args_list
 
     def test_attach_config_and_token_mutually_exclusive(
-        self, _m_getuid, FakeConfig
+        self,
+        _m_getuid,
+        FakeConfig,
     ):
         args = mock.MagicMock(
             token="something", attach_config=FakeFile("something")
@@ -405,7 +416,11 @@ class TestActionAttach:
     @mock.patch(M_PATH + "_post_cli_attach")
     @mock.patch(M_PATH + "actions.attach_with_token")
     def test_token_from_attach_config(
-        self, m_attach_with_token, _m_post_cli_attach, _m_getuid, FakeConfig
+        self,
+        m_attach_with_token,
+        _m_post_cli_attach,
+        _m_getuid,
+        FakeConfig,
     ):
         args = mock.MagicMock(
             token=None,
@@ -418,7 +433,11 @@ class TestActionAttach:
         ] == m_attach_with_token.call_args_list
 
     def test_attach_config_invalid_config(
-        self, _m_getuid, FakeConfig, capsys, event
+        self,
+        _m_getuid,
+        FakeConfig,
+        capsys,
+        event,
     ):
         args = mock.MagicMock(
             token=None,

uaclient/tests/test_cli_status.py
@@ -13,7 +13,9 @@ import yaml
 
 from uaclient import exceptions, messages, status
 from uaclient.cli import action_status, get_parser, main, status_parser
+from uaclient.conftest import FakeNotice
 from uaclient.event_logger import EventLoggerMode
+from uaclient.files.notices import Notice, Notices
 
 M_PATH = "uaclient.cli."
 
@@ -304,7 +306,6 @@ Flags:
 
 
 @mock.patch("uaclient.cli.contract.is_contract_changed", return_value=False)
-@mock.patch("uaclient.files.NoticeFile.remove")
 @mock.patch("uaclient.system.should_reboot", return_value=False)
 @mock.patch(
     "uaclient.status.get_available_resources",
@@ -322,7 +323,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_available_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         capsys,
         FakeConfig,
@@ -343,7 +343,10 @@ class TestActionStatus:
         (
             ([], ""),
             (
-                [["a", "adesc"], ["b2", "bdesc"]],
+                [
+                    [FakeNotice.a, "adesc"],
+                    [FakeNotice.b, "bdesc"],
+                ],
                 "\nNOTICES\n a: adesc\nb2: bdesc\n",
             ),
         ),
@@ -359,26 +362,27 @@ class TestActionStatus:
         ),
     )
     @mock.patch("uaclient.status.format_expires", return_value="formatteddate")
-    def test_attached(
+    def test_attached1(
         self,
+        _m_format_expires,
         _m_getuid,
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
-        _m_format_expires,
-        notices,
-        notice_status,
         features,
         feature_status,
+        notices,
+        notice_status,
         use_all,
         capsys,
         FakeConfig,
     ):
         """Check that root and non-root will emit attached status"""
         cfg = FakeConfig.for_attached_machine()
-        cfg.write_cache("notices", notices)
+        mock_notice = Notices()
+        for notice in notices:
+            mock_notice.add(True, notice[0], notice[1])
         with mock.patch(
             "uaclient.config.UAConfig.features",
             new_callable=mock.PropertyMock,
@@ -423,7 +427,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         use_all,
         capsys,
@@ -451,7 +454,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         use_all,
         capsys,
@@ -479,20 +481,20 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         capsys,
         FakeConfig,
     ):
         """Check that --wait will will block and poll until lock released."""
         cfg = FakeConfig()
+        mock_notice = Notices()
         lock_file = cfg.data_path("lock")
         cfg.write_cache("lock", "123:pro auto-attach")
 
         def fake_sleep(seconds):
             if m_sleep.call_count == 3:
                 os.unlink(lock_file)
-                os.unlink(cfg.notice_file.file.path)
+                mock_notice.remove(True, Notice.OPERATION_IN_PROGRESS)
 
         m_sleep.side_effect = fake_sleep
 
@@ -531,7 +533,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         environ,
         format_type,
@@ -637,7 +638,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         use_all,
         environ,
@@ -767,7 +767,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         use_all,
         format_type,
@@ -910,7 +909,6 @@ class TestActionStatus:
         _m_get_contract_information,
         m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         FakeConfig,
     ):
@@ -934,13 +932,12 @@ class TestActionStatus:
     @mock.patch("uaclient.status.format_expires", return_value="formatteddate")
     def test_unicode_dash_replacement_when_unprintable(
         self,
+        _m_format_expires,
         _m_getuid,
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
-        _m_format_expires,
         encoding,
         expected_dash,
         use_all,
@@ -950,11 +947,12 @@ class TestActionStatus:
         # encoding accurately in older versions of pytest
         underlying_stdout = io.BytesIO()
         fake_stdout = io.TextIOWrapper(underlying_stdout, encoding=encoding)
+        cfg = FakeConfig.for_attached_machine()
 
         with mock.patch("sys.stdout", fake_stdout):
             action_status(
                 mock.MagicMock(all=use_all, simulate_with_token=None),
-                cfg=FakeConfig.for_attached_machine(),
+                cfg=cfg,
             )
 
         fake_stdout.flush()  # Make sure all output is in underlying_stdout
@@ -999,7 +997,6 @@ class TestActionStatus:
         m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         exception_to_throw,
         exception_type,
@@ -1050,7 +1047,6 @@ class TestActionStatus:
         m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         format_type,
         event_logger_mode,
@@ -1072,7 +1068,6 @@ class TestActionStatus:
         m_get_contract_information.side_effect = contract_info_side_effect
 
         cfg = FakeConfig()
-
         args = mock.MagicMock(
             format=format_type, all=False, simulate_with_token=token_to_use
         )
@@ -1098,17 +1093,16 @@ class TestActionStatus:
             (False, False),
         ),
     )
-    @mock.patch("uaclient.files.NoticeFile.try_remove")
-    @mock.patch("uaclient.files.NoticeFile.add")
+    @mock.patch("uaclient.files.notices.Notices.remove")
+    @mock.patch("uaclient.files.notices.Notices.add")
     def test_is_contract_changed(
         self,
         m_add_notice,
-        m_try_remove_notice,
+        m_remove_notice,
         _m_getuid,
         _m_get_contract_information,
         _m_get_available_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         contract_changed,
         is_attached,
@@ -1128,15 +1122,24 @@ class TestActionStatus:
         if is_attached:
             if contract_changed:
                 assert [
-                    mock.call("", messages.NOTICE_REFRESH_CONTRACT_WARNING)
+                    mock.call(
+                        True,
+                        Notice.CONTRACT_REFRESH_WARNING,
+                        messages.NOTICE_REFRESH_CONTRACT_WARNING,
+                    )
                 ] == m_add_notice.call_args_list
             else:
                 assert [
-                    mock.call("", messages.NOTICE_REFRESH_CONTRACT_WARNING)
+                    mock.call(
+                        True,
+                        Notice.CONTRACT_REFRESH_WARNING,
+                        messages.NOTICE_REFRESH_CONTRACT_WARNING,
+                    )
                 ] not in m_add_notice.call_args_list
+                print(_m_contract_changed.return_value)
                 assert [
-                    mock.call("", messages.NOTICE_REFRESH_CONTRACT_WARNING)
-                ] in m_try_remove_notice.call_args_list
+                    mock.call(True, Notice.CONTRACT_REFRESH_WARNING)
+                ] in m_remove_notice.call_args_list
         else:
             assert _m_contract_changed.call_count == 0
 

canonical/ubuntu-pro-client

Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

uaclient/tests/test_cli.py
@@ -31,6 +31,7 @@ from uaclient.exceptions import (
     UnattachedError,
     UserFacingError,
 )
+from uaclient.files.notices import Notice
 
 BIG_DESC = "123456789 " * 7 + "next line"
 BIG_URL = "http://" + "adsf" * 10
@@ -357,13 +358,13 @@ M_PATH_UACONFIG = "uaclient.config.UAConfig."
 class TestAssertLockFile:
     @mock.patch("os.getpid", return_value=123)
     @mock.patch(M_PATH_UACONFIG + "delete_cache_key")
-    @mock.patch("uaclient.files.NoticeFile.add")
+    @mock.patch("uaclient.files.notices.NoticesManager.add")
     @mock.patch(M_PATH_UACONFIG + "write_cache")
     def test_assert_root_creates_lock_and_notice(
         self,
         m_write_cache,
         m_add_notice,
-        m_remove_notice,
+        m_delete_cache,
         _m_getpid,
         FakeConfig,
     ):
@@ -379,8 +380,10 @@ class TestAssertLockFile:
         ret = test_function(arg, cfg=FakeConfig())
         assert mock.sentinel.success == ret
         lock_msg = "Operation in progress: some operation"
-        assert [mock.call("", lock_msg)] == m_add_notice.call_args_list
-        assert [mock.call("lock")] == m_remove_notice.call_args_list
+        assert [
+            mock.call(True, Notice.OPERATION_IN_PROGRESS, lock_msg)
+        ] == m_add_notice.call_args_list
+        assert [mock.call("lock")] == m_delete_cache.call_args_list
         assert [
             mock.call("lock", "123:some operation")
         ] == m_write_cache.call_args_list

uaclient/tests/test_cli_attach.py
@@ -181,7 +181,12 @@ class TestActionAttach:
 
     @mock.patch("uaclient.system.subp")
     def test_lock_file_exists(
-        self, m_subp, _m_getuid, capsys, FakeConfig, event
+        self,
+        m_subp,
+        _m_getuid,
+        capsys,
+        FakeConfig,
+        event,
     ):
         """Check when an operation holds a lock file, attach cannot run."""
         cfg = FakeConfig()
@@ -233,7 +238,7 @@ class TestActionAttach:
         ),
     )
     @mock.patch("uaclient.system.should_reboot", return_value=False)
-    @mock.patch("uaclient.files.NoticeFile.remove")
+    @mock.patch("uaclient.files.notices.NoticesManager.remove")
     @mock.patch("uaclient.status.get_available_resources")
     @mock.patch("uaclient.jobs.update_messaging.update_apt_and_motd_messages")
     @mock.patch(M_PATH + "contract.request_updated_contract")
@@ -242,8 +247,8 @@ class TestActionAttach:
         request_updated_contract,
         m_update_apt_and_motd_msgs,
         _m_get_available_resources,
-        _m_should_reboot,
         _m_remove_notice,
+        _m_should_reboot,
         _m_get_uid,
         error_class,
         error_str,
@@ -275,7 +280,7 @@ class TestActionAttach:
         assert [mock.call(cfg)] == m_update_apt_and_motd_msgs.call_args_list
 
     @mock.patch("uaclient.system.should_reboot", return_value=False)
-    @mock.patch("uaclient.files.NoticeFile.remove")
+    @mock.patch("uaclient.files.notices.NoticesManager.remove")
     @mock.patch("uaclient.jobs.update_messaging.update_apt_and_motd_messages")
     @mock.patch(
         M_PATH + "contract.UAContractClient.request_contract_machine_attach"
@@ -288,8 +293,8 @@ class TestActionAttach:
         m_status,
         contract_machine_attach,
         m_update_apt_and_motd_msgs,
-        _m_should_reboot,
         _m_remove_notice,
+        _m_should_reboot,
         _m_getuid,
         FakeConfig,
         event,
@@ -350,15 +355,15 @@ class TestActionAttach:
 
     @pytest.mark.parametrize("auto_enable", (True, False))
     @mock.patch("uaclient.system.should_reboot", return_value=False)
-    @mock.patch("uaclient.files.NoticeFile.remove")
+    @mock.patch("uaclient.files.notices.NoticesManager.remove")
     @mock.patch("uaclient.status.get_available_resources")
     @mock.patch("uaclient.jobs.update_messaging.update_apt_and_motd_messages")
     def test_auto_enable_passed_through_to_request_updated_contract(
         self,
         m_update_apt_and_motd_msgs,
         _m_get_available_resources,
-        _m_should_reboot,
         _m_remove_notice,
+        _m_should_reboot,
         _m_get_uid,
         auto_enable,
         FakeConfig,
@@ -379,7 +384,9 @@ class TestActionAttach:
         assert [mock.call(cfg)] == m_update_apt_and_motd_msgs.call_args_list
 
     def test_attach_config_and_token_mutually_exclusive(
-        self, _m_getuid, FakeConfig
+        self,
+        _m_getuid,
+        FakeConfig,
     ):
         args = mock.MagicMock(
             token="something", attach_config=FakeFile("something")
@@ -392,7 +399,11 @@ class TestActionAttach:
     @mock.patch(M_PATH + "_post_cli_attach")
     @mock.patch(M_PATH + "actions.attach_with_token")
     def test_token_from_attach_config(
-        self, m_attach_with_token, _m_post_cli_attach, _m_getuid, FakeConfig
+        self,
+        m_attach_with_token,
+        _m_post_cli_attach,
+        _m_getuid,
+        FakeConfig,
     ):
         args = mock.MagicMock(
             token=None,
@@ -405,7 +416,11 @@ class TestActionAttach:
         ] == m_attach_with_token.call_args_list
 
     def test_attach_config_invalid_config(
-        self, _m_getuid, FakeConfig, capsys, event
+        self,
+        _m_getuid,
+        FakeConfig,
+        capsys,
+        event,
     ):
         args = mock.MagicMock(
             token=None,

uaclient/tests/test_cli_status.py
@@ -13,7 +13,9 @@ import yaml
 
 from uaclient import exceptions, messages, status
 from uaclient.cli import action_status, get_parser, main, status_parser
+from uaclient.conftest import FakeNotice
 from uaclient.event_logger import EventLoggerMode
+from uaclient.files.notices import Notice, NoticesManager
 
 M_PATH = "uaclient.cli."
 
@@ -316,7 +318,6 @@ Flags:
 
 
 @mock.patch("uaclient.cli.contract.is_contract_changed", return_value=False)
-@mock.patch("uaclient.files.NoticeFile.remove")
 @mock.patch("uaclient.system.should_reboot", return_value=False)
 @mock.patch(
     "uaclient.status.get_available_resources",
@@ -334,7 +335,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_available_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         capsys,
         FakeConfig,
@@ -355,8 +355,11 @@ class TestActionStatus:
         (
             ([], ""),
             (
-                [["a", "adesc"], ["b2", "bdesc"]],
-                "\nNOTICES\n a: adesc\nb2: bdesc\n",
+                [
+                    [FakeNotice.a, "adesc"],
+                    [FakeNotice.b, "bdesc"],
+                ],
+                "\nNOTICES\nadesc\nbdesc\n",
             ),
         ),
     )
@@ -373,24 +376,25 @@ class TestActionStatus:
     @mock.patch("uaclient.status.format_expires", return_value="formatteddate")
     def test_attached(
         self,
+        _m_format_expires,
         _m_getuid,
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
-        _m_format_expires,
-        notices,
-        notice_status,
         features,
         feature_status,
+        notices,
+        notice_status,
         use_all,
         capsys,
         FakeConfig,
     ):
         """Check that root and non-root will emit attached status"""
         cfg = FakeConfig.for_attached_machine()
-        cfg.write_cache("notices", notices)
+        mock_notice = NoticesManager()
+        for notice in notices:
+            mock_notice.add(True, notice[0], notice[1])
         with mock.patch(
             "uaclient.config.UAConfig.features",
             new_callable=mock.PropertyMock,
@@ -435,7 +439,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         use_all,
         capsys,
@@ -463,7 +466,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         use_all,
         capsys,
@@ -491,20 +493,20 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         capsys,
         FakeConfig,
     ):
         """Check that --wait will will block and poll until lock released."""
         cfg = FakeConfig()
+        mock_notice = NoticesManager()
         lock_file = cfg.data_path("lock")
         cfg.write_cache("lock", "123:pro auto-attach")
 
         def fake_sleep(seconds):
             if m_sleep.call_count == 3:
                 os.unlink(lock_file)
-                os.unlink(cfg.notice_file.file.path)
+                mock_notice.remove(True, Notice.OPERATION_IN_PROGRESS)
 
         m_sleep.side_effect = fake_sleep
 
@@ -543,7 +545,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         environ,
         format_type,
@@ -649,7 +650,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         use_all,
         environ,
@@ -779,7 +779,6 @@ class TestActionStatus:
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         use_all,
         format_type,
@@ -918,7 +917,6 @@ class TestActionStatus:
         _m_get_contract_information,
         m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         FakeConfig,
     ):
@@ -942,13 +940,12 @@ class TestActionStatus:
     @mock.patch("uaclient.status.format_expires", return_value="formatteddate")
     def test_unicode_dash_replacement_when_unprintable(
         self,
+        _m_format_expires,
         _m_getuid,
         _m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
-        _m_format_expires,
         encoding,
         expected_dash,
         use_all,
@@ -958,11 +955,12 @@ class TestActionStatus:
         # encoding accurately in older versions of pytest
         underlying_stdout = io.BytesIO()
         fake_stdout = io.TextIOWrapper(underlying_stdout, encoding=encoding)
+        cfg = FakeConfig.for_attached_machine()
 
         with mock.patch("sys.stdout", fake_stdout):
             action_status(
                 mock.MagicMock(all=use_all, simulate_with_token=None),
-                cfg=FakeConfig.for_attached_machine(),
+                cfg=cfg,
             )
 
         fake_stdout.flush()  # Make sure all output is in underlying_stdout
@@ -1007,7 +1005,6 @@ class TestActionStatus:
         m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         exception_to_throw,
         exception_type,
@@ -1058,7 +1055,6 @@ class TestActionStatus:
         m_get_contract_information,
         _m_get_avail_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         format_type,
         event_logger_mode,
@@ -1080,7 +1076,6 @@ class TestActionStatus:
         m_get_contract_information.side_effect = contract_info_side_effect
 
         cfg = FakeConfig()
-
         args = mock.MagicMock(
             format=format_type, all=False, simulate_with_token=token_to_use
         )
@@ -1106,17 +1101,16 @@ class TestActionStatus:
             (False, False),
         ),
     )
-    @mock.patch("uaclient.files.NoticeFile.try_remove")
-    @mock.patch("uaclient.files.NoticeFile.add")
+    @mock.patch("uaclient.files.notices.NoticesManager.remove")
+    @mock.patch("uaclient.files.notices.NoticesManager.add")
     def test_is_contract_changed(
         self,
         m_add_notice,
-        m_try_remove_notice,
+        m_remove_notice,
         _m_getuid,
         _m_get_contract_information,
         _m_get_available_resources,
         _m_should_reboot,
-        _m_remove_notice,
         _m_contract_changed,
         contract_changed,
         is_attached,
@@ -1136,15 +1130,24 @@ class TestActionStatus:
         if is_attached:
             if contract_changed:
                 assert [
-                    mock.call("", messages.NOTICE_REFRESH_CONTRACT_WARNING)
+                    mock.call(
+                        True,
+                        Notice.CONTRACT_REFRESH_WARNING,
+                        messages.NOTICE_REFRESH_CONTRACT_WARNING,
+                    )
                 ] == m_add_notice.call_args_list
             else:
                 assert [
-                    mock.call("", messages.NOTICE_REFRESH_CONTRACT_WARNING)
+                    mock.call(
+                        True,
+                        Notice.CONTRACT_REFRESH_WARNING,
+                        messages.NOTICE_REFRESH_CONTRACT_WARNING,
+                    )
                 ] not in m_add_notice.call_args_list
+                print(_m_contract_changed.return_value)
                 assert [
-                    mock.call("", messages.NOTICE_REFRESH_CONTRACT_WARNING)
-                ] in m_try_remove_notice.call_args_list
+                    mock.call(True, Notice.CONTRACT_REFRESH_WARNING)
+                ] in m_remove_notice.call_args_list
         else:
             assert _m_contract_changed.call_count == 0
 