21327c441fd9d682f399941c9617ae6f5e34065c
Renan Rodrigo <renanrodrigo@canonical.com>
2023-04-04 11:38:49 -0300

    security-status: change message hint to package information
    Instead of apt-cache policy, the suggestion is to look at apt-cache
    show - which brings way more information about a specific package. The
    hint is only shown if there is an update, whether applied or available,
    preferring packages from the latter.
    
    Fixes: #2443
    
    Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

--- uaclient/messages.py
+++ uaclient/messages.py
@@ -1056,9 +1056,9 @@ Learn more at {url}
     url=BASE_UA_URL
 )
 
-SS_POLICY_HINT = """\
+SS_SHOW_HINT = """\
 For example, run:
-    apt-cache policy {package}
+    apt-cache show {package}
 to learn more about that package."""
 
 SS_NO_THIRD_PARTY = "You have no packages installed from a third party."
@@ -1071,7 +1071,7 @@ SS_SERVICE_HELP = "Run 'pro help {service}' to learn more"
 
 SS_UPDATES_AVAILABLE = "Installed packages with an available {service} update:"
 SS_UPDATES_INSTALLED = "Installed packages with an {service} update applied:"
-SS_OTHER_PACKAGES = "Further installed packages covered by {service}:"
+SS_OTHER_PACKAGES = "{prefix} packages covered by {service}:"
 
 ENTITLEMENT_NOT_FOUND = FormattedNamedMessage(
     "entitlement-not-found",
