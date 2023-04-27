3a374c872556fa39c4347b366f086715c0633445
Grant Orndorff <grant.orndorff@canonical.com>
2023-04-27 16:51:47 -0400

    new release 27.15

--- uaclient/version.py
+++ uaclient/version.py
@@ -15,7 +15,7 @@ from uaclient.defaults import CANDIDATE_CACHE_PATH, UAC_RUN_PATH
 from uaclient.exceptions import ProcessExecutionError
 from uaclient.system import subp
 
-__VERSION__ = "27.14.4"
+__VERSION__ = "27.15"
 PACKAGED_VERSION = "@@PACKAGED_VERSION@@"
 
 CANDIDATE_REGEX = r"Candidate: (?P<candidate>.*?)\n"
