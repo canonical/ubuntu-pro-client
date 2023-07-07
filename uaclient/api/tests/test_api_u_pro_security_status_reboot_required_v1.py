86aed291de22542e342075546f14b6f574ff0eeb
Lucas Moura <lucas.moura@canonical.com>
2023-07-07 10:07:36 +0000

    reboot_required: add more data to endpoint
    We are adding extra data to the reboot required endpoint to allow
    user to better understand the decision making regarding if a reboot
    is required or not

--- uaclient/api/tests/test_api_u_pro_security_status_reboot_required_v1.py
+++ uaclient/api/tests/test_api_u_pro_security_status_reboot_required_v1.py
@@ -4,24 +4,124 @@ import pytest
 from uaclient.api.u.pro.security.status.reboot_required.v1 import (
     _reboot_required,
 )
+from uaclient.livepatch import LivepatchPatchStatus, LivepatchStatusStatus
 from uaclient.security_status import RebootStatus
+from uaclient.system import RebootRequiredPkgs
 
 PATH = "uaclient.api.u.pro.security.status.reboot_required.v1."
 
 
 class TestRebootStatus:
     @pytest.mark.parametrize(
-        "reboot_state",
         (
-            (RebootStatus.REBOOT_REQUIRED),
-            (RebootStatus.REBOOT_NOT_REQUIRED),
-            (RebootStatus.REBOOT_REQUIRED_LIVEPATCH_APPLIED),
+            "reboot_state,reboot_required_pkgs,livepatch_status,"
+            "expected_standard_pkgs,expected_kernel_pkgs,"
+            "expected_livepatch_enabled_and_kernel,"
+            "expected_livepatch_enabled,expected_livepatch_state,"
+            "expected_livepatch_support"
+        ),
+        (
+            (
+                RebootStatus.REBOOT_REQUIRED,
+                RebootRequiredPkgs(
+                    standard_packages=["pkg1"], kernel_packages=["linux-base"]
+                ),
+                LivepatchStatusStatus(
+                    kernel=None,
+                    livepatch=LivepatchPatchStatus(
+                        state="nothing-to-apply", fixes=None, version=None
+                    ),
+                    supported="supported",
+                ),
+                ["pkg1"],
+                ["linux-base"],
+                True,
+                True,
+                "nothing-to-apply",
+                "supported",
+            ),
+            (
+                RebootStatus.REBOOT_NOT_REQUIRED,
+                None,
+                None,
+                None,
+                None,
+                False,
+                False,
+                None,
+                None,
+            ),
+            (
+                RebootStatus.REBOOT_REQUIRED_LIVEPATCH_APPLIED,
+                RebootRequiredPkgs(
+                    standard_packages=[], kernel_packages=["linux-base"]
+                ),
+                LivepatchStatusStatus(
+                    kernel=None,
+                    livepatch=LivepatchPatchStatus(
+                        state="applied", fixes=None, version=None
+                    ),
+                    supported="supported",
+                ),
+                [],
+                ["linux-base"],
+                True,
+                True,
+                "applied",
+                "supported",
+            ),
+            (
+                RebootStatus.REBOOT_REQUIRED_LIVEPATCH_APPLIED,
+                RebootRequiredPkgs(
+                    standard_packages=[], kernel_packages=["linux-base"]
+                ),
+                LivepatchStatusStatus(
+                    kernel=None,
+                    livepatch=LivepatchPatchStatus(
+                        state="unknown", fixes=None, version=None
+                    ),
+                    supported="unknown",
+                ),
+                [],
+                ["linux-base"],
+                False,
+                True,
+                "unknown",
+                "unknown",
+            ),
         ),
     )
+    @mock.patch(PATH + "status")
+    @mock.patch(PATH + "get_reboot_required_pkgs")
     @mock.patch(PATH + "get_reboot_status")
-    def test_reboot_status_api(self, m_get_reboot_status, reboot_state):
+    def test_reboot_status_api(
+        self,
+        m_get_reboot_status,
+        m_get_reboot_required_pkgs,
+        m_livepatch_status,
+        reboot_state,
+        reboot_required_pkgs,
+        livepatch_status,
+        expected_standard_pkgs,
+        expected_kernel_pkgs,
+        expected_livepatch_enabled_and_kernel,
+        expected_livepatch_enabled,
+        expected_livepatch_state,
+        expected_livepatch_support,
+    ):
         m_get_reboot_status.return_value = reboot_state
+        m_get_reboot_required_pkgs.return_value = reboot_required_pkgs
+        m_livepatch_status.return_value = livepatch_status
+        result = _reboot_required(mock.MagicMock())
+        assert reboot_state.value == result.reboot_required
         assert (
-            reboot_state.value
-            == _reboot_required(mock.MagicMock()).reboot_required
+            expected_standard_pkgs
+            == result.reboot_required_packages.standard_packages
         )
+        assert (
+            expected_livepatch_enabled_and_kernel
+            == result.livepatch_enabled_and_kernel_patched
+        )
+        assert expected_livepatch_enabled == result.livepatch_enabled
+        assert expected_livepatch_state == result.livepatch_state
+        assert expected_livepatch_support == result.livepatch_support
--- uaclient/api/u/pro/security/status/reboot_required/v1.py
+++ uaclient/api/u/pro/security/status/reboot_required/v1.py
@@ -1,20 +1,64 @@
+from typing import List, Optional
+
 from uaclient.api.api import APIEndpoint
 from uaclient.api.data_types import AdditionalInfo
 from uaclient.config import UAConfig
-from uaclient.data_types import DataObject, Field, StringDataValue
-from uaclient.security_status import get_reboot_status
+from uaclient.data_types import (
+    BoolDataValue,
+    DataObject,
+    Field,
+    StringDataValue,
+    data_list,
+)
+from uaclient.livepatch import status
+from uaclient.security_status import (
+    get_reboot_required_pkgs,
+    get_reboot_status,
+)
+
+
+class RebootRequiredPkgs(DataObject):
+    fields = [
+        Field("standard_packages", data_list(StringDataValue), False),
+        Field("kernel_packages", data_list(StringDataValue), False),
+    ]
+
+    def __init__(
+        self,
+        standard_packages: Optional[List[str]],
+        kernel_packages: Optional[List[str]],
+    ):
+        self.standard_packages = standard_packages
+        self.kernel_packages = kernel_packages
 
 
 class RebootRequiredResult(DataObject, AdditionalInfo):
     fields = [
         Field("reboot_required", StringDataValue),
+        Field("reboot_required_packages", RebootRequiredPkgs),
+        Field("livepatch_enabled_and_kernel_patched", BoolDataValue),
+        Field("livepatch_enabled", BoolDataValue),
+        Field("livepatch_state", StringDataValue, False),
+        Field("livepatch_support", StringDataValue, False),
     ]
 
     def __init__(
         self,
         reboot_required: str,
+        reboot_required_packages: RebootRequiredPkgs,
+        livepatch_enabled_and_kernel_patched: bool,
+        livepatch_enabled: bool,
+        livepatch_state: Optional[str],
+        livepatch_support: Optional[str],
     ):
         self.reboot_required = reboot_required
+        self.reboot_required_packages = reboot_required_packages
+        self.livepatch_enabled_and_kernel_patched = (
+            livepatch_enabled_and_kernel_patched
+        )
+        self.livepatch_enabled = livepatch_enabled
+        self.livepatch_state = livepatch_state
+        self.livepatch_support = livepatch_support
 
 
 def reboot_required() -> RebootRequiredResult:
@@ -22,8 +66,46 @@ def reboot_required() -> RebootRequiredResult:
 
 
 def _reboot_required(cfg: UAConfig) -> RebootRequiredResult:
-    status = get_reboot_status()
-    return RebootRequiredResult(reboot_required=status.value)
+    reboot_status = get_reboot_status()
+    reboot_required_pkgs = get_reboot_required_pkgs()
+    livepatch_status = status()
+
+    if not livepatch_status:
+        livepatch_enabled_and_kernel_patched = False
+        livepatch_enabled = False
+        livepatch_state = None
+        livepatch_support = None
+    else:
+        livepatch_enabled = True
+        livepatch_support = livepatch_status.supported
+        livepatch_state = (
+            livepatch_status.livepatch.state
+            if livepatch_status.livepatch
+            else None
+        )
+        if (
+            livepatch_state not in ("applied", "nothing-to-apply")
+            and livepatch_support != "supported"
+        ):
+            livepatch_enabled_and_kernel_patched = False
+        else:
+            livepatch_enabled_and_kernel_patched = True
+
+    return RebootRequiredResult(
+        reboot_required=reboot_status.value,
+        reboot_required_packages=RebootRequiredPkgs(
+            standard_packages=reboot_required_pkgs.standard_packages
+            if reboot_required_pkgs
+            else None,
+            kernel_packages=reboot_required_pkgs.kernel_packages
+            if reboot_required_pkgs
+            else None,
+        ),
+        livepatch_enabled_and_kernel_patched=livepatch_enabled_and_kernel_patched,  # noqa
+        livepatch_enabled=livepatch_enabled,
+        livepatch_state=livepatch_state,
+        livepatch_support=livepatch_support,
+    )
 
 
 endpoint = APIEndpoint(
