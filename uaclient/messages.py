88e1387ddf6e30cdc7ae9c3f84800e9f067621bc
Grant Orndorff <grant.orndorff@canonical.com>
2023-07-24 17:01:22 -0400

    anbox: remove duplicate phrase in messaging

--- uaclient/messages.py
+++ uaclient/messages.py
@@ -1380,8 +1380,7 @@ $ sudo anbox-cloud-appliance init
 
 You can accept the default answers if you do not have any specific
 configuration changes.
-For more information, see
-https://anbox-cloud.io/docs/tut/installing-appliance for more information.
+For more information, see https://anbox-cloud.io/docs/tut/installing-appliance
 """,
 )
 
