b2086967193c0684f7494c09af592bf07f6d17ed
Grant Orndorff <grant.orndorff@canonical.com>
2023-07-24 12:01:56 -0400

    http: configure web proxies on all entrypoints that make http requests

--- lib/upgrade_lts_contract.py
+++ lib/upgrade_lts_contract.py
@@ -7,12 +7,13 @@ See uaclient/upgrade_lts_contract.py for more details.
 
 import logging
 
-from uaclient import upgrade_lts_contract
+from uaclient import http, upgrade_lts_contract
 from uaclient.cli import setup_logging
 from uaclient.config import UAConfig
 
 if __name__ == "__main__":
     setup_logging(logging.INFO, logging.DEBUG)
     cfg = UAConfig()
+    http.configure_web_proxy(cfg.http_proxy, cfg.https_proxy)
     upgrade_lts_contract.process_contract_delta_after_apt_lock(cfg)
     upgrade_lts_contract.remove_private_esm_apt_cache()
