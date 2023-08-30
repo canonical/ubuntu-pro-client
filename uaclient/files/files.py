234b053ee10b4cf807353a1e03e20be1c9f18937
Christian Ehrhardt <christian.ehrhardt@canonical.com>
2023-08-30 08:47:17 +0200

    config: unify conflicting PRIVATE_SUBDIR definitions
    We had conflicting definitions of the same name PRIVATE_SUBDIR and
    due to that some places using it in one or the other way from one
    of the two sources.
    
    Furthermore the definitions were different, once with and once without
    a leading slash.
    
    This commit unifies this to one place defining the string in just one way
    (no leading slash) and adapts users that assumed differently.
    
    Signed-off-by: Christian Ehrhardt <christian.ehrhardt@canonical.com>

--- uaclient/files/files.py
+++ uaclient/files/files.py
@@ -75,7 +75,7 @@ class MachineTokenFile:
     ):
         file_name = defaults.MACHINE_TOKEN_FILE
         self.private_file = UAFile(
-            file_name, directory + defaults.PRIVATE_SUBDIR
+            file_name, directory + "/" + defaults.PRIVATE_SUBDIR
         )
         self.public_file = UAFile(file_name, directory, False)
         self.machine_token_overlay_path = machine_token_overlay_path
