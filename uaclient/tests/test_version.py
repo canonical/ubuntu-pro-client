9f7b3eaab9bad1a14c824bdab4d647fa0eacf1fa
Renan Rodrigo <renanrodrigo@canonical.com>
2023-09-22 13:26:14 -0300

    version: reuse get_pkg_candidate_version
    Also fix a unit test that was passing based on luck
    
    Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

--- uaclient/tests/test_version.py
+++ uaclient/tests/test_version.py
@@ -41,35 +41,32 @@ class TestGetLastKnownCandidate:
     @mock.patch("builtins.open", mock.mock_open(read_data="1.2.3"))
     @mock.patch("os.stat")
     @mock.patch("os.path.exists", return_value=True)
-    @mock.patch("uaclient.version.get_apt_cache_time", return_value=2)
+    @mock.patch("uaclient.version.get_apt_cache_time", return_value=1)
     def test_get_known_candidate_from_cache(
         self, _m_apt_cache_time, _m_exists, m_stat
     ):
-        m_stat.return_value.st_mtime = 1
+        m_stat.return_value.st_mtime = 2
         assert "1.2.3" == get_last_known_candidate()
 
     @mock.patch("builtins.open")
     @mock.patch("os.stat")
     @mock.patch("os.path.exists", return_value=True)
-    @mock.patch("uaclient.version.get_apt_cache_time", return_value=2)
+    @mock.patch("uaclient.version.get_apt_cache_time", return_value=1)
     def test_cant_open_cache_file(
         self, _m_apt_cache_time, _m_exists, m_stat, m_open
     ):
-        m_stat.return_value.st_mtime = 1
+        m_stat.return_value.st_mtime = 2
         m_open.side_effect = OSError()
         assert None is get_last_known_candidate()
 
     @mock.patch("os.makedirs")
-    @mock.patch("uaclient.version.get_apt_cache_policy_for_package")
+    @mock.patch(
+        "uaclient.version.get_pkg_candidate_version", return_value="1.2.3"
+    )
     @mock.patch("os.path.exists", return_value=False)
     def test_create_cache_before_returning(
-        self, _m_exists, m_policy, _m_makedirs
+        self, _m_exists, _m_candidate, _m_makedirs
     ):
-        m_policy.return_value = """
-            Installed: 1.1.2
-            Candidate: 1.2.3
-            Version table:
-        """
         with mock.patch("builtins.open", mock.mock_open()) as m_open:
             get_last_known_candidate()
             assert m_open.return_value.write.call_args_list == [
@@ -77,14 +74,11 @@ class TestGetLastKnownCandidate:
             ]
 
     @mock.patch("builtins.open")
-    @mock.patch("uaclient.version.get_apt_cache_policy_for_package")
+    @mock.patch(
+        "uaclient.version.get_pkg_candidate_version", return_value="1.2.3"
+    )
     @mock.patch("os.path.exists", return_value=False)
-    def test_problem_updating_file(self, _m_exists, m_policy, m_open):
-        m_policy.return_value = """
-            Installed: 1.1.2
-            Candidate: 1.2.3
-            Version table:
-        """
+    def test_problem_updating_file(self, _m_exists, _m_candidate, m_open):
         m_open.side_effect = OSError()
         assert "1.2.3" == get_last_known_candidate()
 
@@ -92,7 +86,7 @@ class TestGetLastKnownCandidate:
 class TestCheckForNewVersion:
     @pytest.mark.parametrize("compare_return", (-1, 0, 1))
     @mock.patch("uaclient.version.version_compare")
-    @mock.patch("uaclient.version.get_last_known_candidate")
+    @mock.patch("uaclient.version.get_pkg_candidate_version")
     def test_check_for_new_version(
         self, m_candidate, m_compare, compare_return
     ):
