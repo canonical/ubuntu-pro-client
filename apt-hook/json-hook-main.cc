e3f02dd358a734be1239e16eea061e1fd7ac5cc1
Grant Orndorff <grant.orndorff@canonical.com>
2023-09-26 20:34:54 -0400

    apt-hook: refactor to use gettext

--- apt-hook/json-hook-main.cc
+++ apt-hook/json-hook-main.cc
@@ -1,8 +1,12 @@
+#include <libintl.h>
+
 #include "json-hook.hh"
 
 int main(int argc, char *argv[])
 {
     (void) argc;
     (void) argv;
+    setlocale(LC_ALL, "");
+    textdomain("ubuntu-pro");
     return run();
 }
