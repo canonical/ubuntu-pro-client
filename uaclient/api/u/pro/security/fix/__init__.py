6cbe5b90ab9d7de8dec6e10d6141ccb342c827cc
Lucas Moura <lucas.moura@canonical.com>
2023-10-11 15:15:25 -0300

    api: allow plan imports through fix module
    Due to the backwards compatibility, we will still allow
    some plan API related objects to be imported through
    the fix module

---
+++ uaclient/api/u/pro/security/fix/__init__.py
@@ -0,0 +1,25 @@
+from uaclient.api.u.pro.security.fix._common.plan.v1 import (
+    AptUpgradeData,
+    AttachData,
+    EnableData,
+    FixPlanError,
+    FixPlanResult,
+    FixPlanStep,
+    FixPlanWarning,
+    NoOpData,
+    PackageCannotBeInstalledData,
+    SecurityIssueNotFixedData,
+)
+
+__all__ = [
+    "AptUpgradeData",
+    "AttachData",
+    "EnableData",
+    "FixPlanError",
+    "FixPlanResult",
+    "FixPlanStep",
+    "FixPlanWarning",
+    "NoOpData",
+    "PackageCannotBeInstalledData",
+    "SecurityIssueNotFixedData",
+]
