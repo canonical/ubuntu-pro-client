cedcb28ad3d90dae7b1f1d8fa8591788cb39e215
Lucas Moura <lucas.moura@canonical.com>
2023-12-07 16:50:17 -0300

    api: add services with warning to enabled_services
    If a service has has a warning status, we are now returning that
    service as a enabled service through the enabled_services API.
    However, we are also creating a warning in the API response
    to let the user know that there is a required action for the
    service.
    
    Fixes: #2882

--- uaclient/api/tests/test_api_u_pro_status_enabled_services_v1.py
+++ uaclient/api/tests/test_api_u_pro_status_enabled_services_v1.py
@@ -1,11 +1,13 @@
 import mock
 
 from uaclient import entitlements
+from uaclient.api.data_types import ErrorWarningObject
 from uaclient.api.u.pro.status.enabled_services.v1 import (
     EnabledService,
     _enabled_services,
 )
 from uaclient.entitlements.entitlement_status import UserFacingStatus
+from uaclient.messages import NamedMessage
 
 
 class TestEnabledServicesV1:
@@ -52,8 +54,31 @@ class TestEnabledServicesV1:
             UserFacingStatus.INACTIVE,
             "",
         )
+        m_cls_3.return_value = m_inst_3
 
-        ents = [m_cls_1, m_cls_2, m_cls_3]
+        m_cls_4 = mock.MagicMock()
+        m_inst_4 = mock.MagicMock()
+        type(m_inst_4).presentation_name = mock.PropertyMock(
+            return_value="ent4"
+        )
+        m_inst_4.user_facing_status.return_value = (
+            UserFacingStatus.INAPPLICABLE,
+            "",
+        )
+        m_cls_4.return_value = m_inst_4
+
+        m_cls_5 = mock.MagicMock()
+        m_inst_5 = mock.MagicMock()
+        type(m_inst_5).presentation_name = mock.PropertyMock(
+            return_value="ent5"
+        )
+        m_inst_5.user_facing_status.return_value = (
+            UserFacingStatus.WARNING,
+            NamedMessage(name="warning_code", msg="warning_msg"),
+        )
+        m_cls_5.return_value = m_inst_5
+
+        ents = [m_cls_1, m_cls_2, m_cls_3, m_cls_4, m_cls_5]
         expected_enabled_services = [
             EnabledService(name="ent1"),
             EnabledService(
@@ -61,15 +86,25 @@ class TestEnabledServicesV1:
                 variant_enabled=True,
                 variant_name="variant",
             ),
+            EnabledService(name="ent5"),
+        ]
+
+        expected_warnings = [
+            ErrorWarningObject(
+                title="warning_msg",
+                code="warning_code",
+                meta={"service": "ent5"},
+            )
         ]
 
         with mock.patch.object(entitlements, "ENTITLEMENT_CLASSES", ents):
-            actual_enabled_services = _enabled_services(
-                cfg=mock.MagicMock()
-            ).enabled_services
+            enabled_services_ret = _enabled_services(cfg=mock.MagicMock())
 
         assert 1 == m_is_attached.call_count
-        assert expected_enabled_services == actual_enabled_services
+        assert (
+            expected_enabled_services == enabled_services_ret.enabled_services
+        )
+        assert expected_warnings == enabled_services_ret.warnings
 
     @mock.patch("uaclient.api.u.pro.status.enabled_services.v1._is_attached")
     def test_enabled_services_when_unattached(self, m_is_attached):
