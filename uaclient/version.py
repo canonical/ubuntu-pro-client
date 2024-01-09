833f3ec831fbf96f86e08e7759dc5b0b68fc2a29
Grant Orndorff <grant.orndorff@canonical.com>
2024-01-09 15:03:48 -0500

    release 31

--- uaclient/version.py
+++ uaclient/version.py
@@ -14,7 +14,7 @@ from uaclient.defaults import CANDIDATE_CACHE_PATH, UAC_RUN_PATH
 from uaclient.exceptions import ProcessExecutionError
 from uaclient.system import subp
 
-__VERSION__ = "1:1+devel"
+__VERSION__ = "31"
 PACKAGED_VERSION = "@@PACKAGED_VERSION@@"
 
 
