7ed70de6c5f5038d9f9d4d28b1d5ddfddde274f1
Grant Orndorff <grant.orndorff@canonical.com>
2024-02-13 14:53:19 -0500

    livepatch: refresh snapd before installing livepatch

--- uaclient/entitlements/base.py
+++ uaclient/entitlements/base.py
@@ -511,6 +511,16 @@ class UAEntitlement(metaclass=abc.ABCMeta):
 
         snap.run_snapd_wait_cmd()
 
+        try:
+            snap.refresh_snap("snapd")
+        except exceptions.ProcessExecutionError as e:
+            LOG.warning("Failed to refresh snapd snap", exc_info=e)
+            event.info(
+                messages.EXECUTING_COMMAND_FAILED.format(
+                    command="snap refresh snapd"
+                )
+            )
+
         http_proxy = http.validate_proxy(
             "http", self.cfg.http_proxy, http.PROXY_VALIDATION_SNAP_HTTP_URL
         )
