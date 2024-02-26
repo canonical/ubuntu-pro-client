7daf608878fa5f8f8d583344dbd5187b8468cb25
Grant Orndorff <grant.orndorff@canonical.com>
2024-02-26 11:47:46 -0500

    debian: rename logrotate file to new package name
    LP: #2055046

--- uaclient/version.py
+++ uaclient/version.py
@@ -14,7 +14,7 @@ from uaclient.defaults import CANDIDATE_CACHE_PATH, UAC_RUN_PATH
 from uaclient.exceptions import ProcessExecutionError
 from uaclient.system import subp
 
-__VERSION__ = "31.1"
+__VERSION__ = "31.2"
 PACKAGED_VERSION = "@@PACKAGED_VERSION@@"
 
 
