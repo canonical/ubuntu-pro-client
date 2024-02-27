0b52c5d81125e5b4181b3a360b97a9fa6db42340
Lucas Moura <lucas.moura@canonical.com>
2024-02-27 16:21:45 -0300

    fips: remove unecessary calls to write_cache
    There is no need for the fips module to still use the
    write_cache method from the UAConfig object.

--- uaclient/entitlements/fips.py
+++ uaclient/entitlements/fips.py
@@ -634,14 +634,6 @@ class FIPSUpdatesEntitlement(FIPSCommonEntitlement):
 
     def _perform_enable(self, silent: bool = False) -> bool:
         if super()._perform_enable(silent=silent):
-            services_once_enabled = (
-                self.cfg.read_cache("services-once-enabled") or {}
-            )
-            services_once_enabled.update({self.name: True})
-            self.cfg.write_cache(
-                key="services-once-enabled", content=services_once_enabled
-            )
-
             services_once_enabled_file.write(
                 ServicesOnceEnabledData(fips_updates=True)
             )
