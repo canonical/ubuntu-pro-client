ba150d9ecedf4e17ce0d6d47964224ed23bac68a
Lucas Moura <lucas.moura@canonical.com>
2024-03-05 16:27:43 -0300

    files: move machine_token to a separate module
    Create specific module for the machine token file

--- uaclient/files/__init__.py
+++ uaclient/files/__init__.py
@@ -1,5 +1,6 @@
 from uaclient.files.data_types import DataObjectFile, DataObjectFileFormat
-from uaclient.files.files import MachineTokenFile, UAFile
+from uaclient.files.files import UAFile
+from uaclient.files.machine_token import MachineTokenFile
 
 __all__ = [
     "DataObjectFile",
