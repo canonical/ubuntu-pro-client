7b4e68f2f693582978bc0726f8fb3d4355194c7e
Lucas Moura <lucas.moura@canonical.com>
2024-06-20 17:08:41 -0300

    api: add support for usn vulnerabilities
    Add new API u.pro.security.vulnerabilities.usn.v1 that will
    list all of the fixable USNs in the system

---
+++ uaclient/api/tests/test_api_u_pro_security_vulnerabilities_usn_v1.py
@@ -0,0 +1,296 @@
+import datetime
+
+import mock
+import pytest
+
+from uaclient.api.u.pro.security.vulnerabilities.usn.v1 import (
+    USNAffectedPackage,
+    USNVulnerabilitiesOptions,
+    USNVulnerabilitiesResult,
+    USNVulnerabilityResult,
+    _vulnerabilities,
+)
+
+M_PATH = "uaclient.api.u.pro.security.vulnerabilities.usn.v1."
+
+VULNEBILITIES_DATA = {
+    "published_at": "2024-06-24T13:19:16",
+    "packages": {
+        "test1": {
+            "source_versions": {
+                "1.1.2": {
+                    "binary_packages": {
+                        "test1-bin": "1.1.2",
+                        "test1-bin1": "1.1.2",
+                    },
+                    "pocket": "esm-infra",
+                }
+            },
+            "ubuntu_security_notices_regressions": {
+                "USN-2022-1": {
+                    "source_fixed_version": None,
+                },
+            },
+            "ubuntu_security_notices": {
+                "USN-2022-5": {
+                    "source_fixed_version": "1.1.2",
+                },
+                "USN-2022-18": {
+                    "source_fixed_version": "1.0.9",
+                },
+                "USN-2022-9": {
+                    "source_fixed_version": None,
+                },
+            },
+        }
+    },
+    "security_issues": {
+        "usns": {
+            "USN-2022-1": {
+                "description": "description",
+                "published_at": "2024-06-24T13:19:16",
+                "related_cves": [],
+                "related_launchpad_bugs": [
+                    "https://launchpad.net/bugs/BUG_ID"
+                ],
+            },
+            "USN-2022-5": {
+                "description": "description",
+                "published_at": "2024-06-24T13:19:16",
+                "related_cves": ["CVE-2022-12345"],
+                "related_launchpad_bugs": [],
+            },
+            "USN-2022-9": {
+                "description": "description",
+                "published_at": "2024-06-24T13:19:16",
+                "related_cves": ["CVE-2022-12345"],
+                "related_launchpad_bugs": [],
+            },
+        }
+    },
+}
+
+
+INSTALLED_PKGS_BY_SOURCE = {
+    "test1": {
+        "test1-bin1": "1.1.1",
+        "test1-bin": "1.1.1",
+    }
+}
+
+
+class TestUSNVulnerabilities:
+    @pytest.mark.parametrize(
+        "vulnerabilities_data,installed_pkgs_by_source,usn_options,expected_result",  # noqa
+        (
+            (
+                VULNEBILITIES_DATA,
+                INSTALLED_PKGS_BY_SOURCE,
+                USNVulnerabilitiesOptions(all=True),
+                USNVulnerabilitiesResult(
+                    usns=[
+                        USNVulnerabilityResult(
+                            name="USN-2022-1",
+                            description="description",
+                            fixable="no",
+                            affected_packages=[
+                                USNAffectedPackage(
+                                    name="test1-bin",
+                                    current_version="1.1.1",
+                                    fix_version=None,
+                                    fix_available_from=None,
+                                ),
+                                USNAffectedPackage(
+                                    name="test1-bin1",
+                                    current_version="1.1.1",
+                                    fix_version=None,
+                                    fix_available_from=None,
+                                ),
+                            ],
+                            published_at=datetime.datetime(
+                                2024, 6, 24, 13, 19, 16
+                            ),
+                            related_cves=[],
+                            related_launchpad_bugs=[
+                                "https://launchpad.net/bugs/BUG_ID"
+                            ],
+                        ),
+                        USNVulnerabilityResult(
+                            name="USN-2022-5",
+                            description="description",
+                            fixable="yes",
+                            affected_packages=[
+                                USNAffectedPackage(
+                                    name="test1-bin",
+                                    current_version="1.1.1",
+                                    fix_version="1.1.2",
+                                    fix_available_from="esm-infra",
+                                ),
+                                USNAffectedPackage(
+                                    name="test1-bin1",
+                                    current_version="1.1.1",
+                                    fix_version="1.1.2",
+                                    fix_available_from="esm-infra",
+                                ),
+                            ],
+                            published_at=datetime.datetime(
+                                2024, 6, 24, 13, 19, 16
+                            ),
+                            related_cves=["CVE-2022-12345"],
+                            related_launchpad_bugs=[],
+                        ),
+                        USNVulnerabilityResult(
+                            name="USN-2022-9",
+                            description="description",
+                            fixable="no",
+                            affected_packages=[
+                                USNAffectedPackage(
+                                    name="test1-bin",
+                                    current_version="1.1.1",
+                                    fix_version=None,
+                                    fix_available_from=None,
+                                ),
+                                USNAffectedPackage(
+                                    name="test1-bin1",
+                                    current_version="1.1.1",
+                                    fix_version=None,
+                                    fix_available_from=None,
+                                ),
+                            ],
+                            published_at=datetime.datetime(
+                                2024, 6, 24, 13, 19, 16
+                            ),
+                            related_cves=["CVE-2022-12345"],
+                            related_launchpad_bugs=[],
+                        ),
+                    ],
+                    vulnerability_data_published_at=datetime.datetime(
+                        2024, 6, 24, 13, 19, 16
+                    ),
+                    apt_updated_at=datetime.datetime(2024, 6, 24, 13, 19, 16),
+                ),
+            ),
+            (
+                VULNEBILITIES_DATA,
+                INSTALLED_PKGS_BY_SOURCE,
+                USNVulnerabilitiesOptions(),
+                USNVulnerabilitiesResult(
+                    usns=[
+                        USNVulnerabilityResult(
+                            name="USN-2022-5",
+                            description="description",
+                            fixable="yes",
+                            affected_packages=[
+                                USNAffectedPackage(
+                                    name="test1-bin",
+                                    current_version="1.1.1",
+                                    fix_version="1.1.2",
+                                    fix_available_from="esm-infra",
+                                ),
+                                USNAffectedPackage(
+                                    name="test1-bin1",
+                                    current_version="1.1.1",
+                                    fix_version="1.1.2",
+                                    fix_available_from="esm-infra",
+                                ),
+                            ],
+                            published_at=datetime.datetime(
+                                2024, 6, 24, 13, 19, 16
+                            ),
+                            related_cves=["CVE-2022-12345"],
+                            related_launchpad_bugs=[],
+                        ),
+                    ],
+                    vulnerability_data_published_at=datetime.datetime(
+                        2024, 6, 24, 13, 19, 16
+                    ),
+                    apt_updated_at=datetime.datetime(2024, 6, 24, 13, 19, 16),
+                ),
+            ),
+            (
+                VULNEBILITIES_DATA,
+                INSTALLED_PKGS_BY_SOURCE,
+                USNVulnerabilitiesOptions(unfixable=True),
+                USNVulnerabilitiesResult(
+                    usns=[
+                        USNVulnerabilityResult(
+                            name="USN-2022-1",
+                            description="description",
+                            fixable="no",
+                            affected_packages=[
+                                USNAffectedPackage(
+                                    name="test1-bin",
+                                    current_version="1.1.1",
+                                    fix_version=None,
+                                    fix_available_from=None,
+                                ),
+                                USNAffectedPackage(
+                                    name="test1-bin1",
+                                    current_version="1.1.1",
+                                    fix_version=None,
+                                    fix_available_from=None,
+                                ),
+                            ],
+                            published_at=datetime.datetime(
+                                2024, 6, 24, 13, 19, 16
+                            ),
+                            related_cves=[],
+                            related_launchpad_bugs=[
+                                "https://launchpad.net/bugs/BUG_ID"
+                            ],
+                        ),
+                        USNVulnerabilityResult(
+                            name="USN-2022-9",
+                            description="description",
+                            fixable="no",
+                            affected_packages=[
+                                USNAffectedPackage(
+                                    name="test1-bin",
+                                    current_version="1.1.1",
+                                    fix_version=None,
+                                    fix_available_from=None,
+                                ),
+                                USNAffectedPackage(
+                                    name="test1-bin1",
+                                    current_version="1.1.1",
+                                    fix_version=None,
+                                    fix_available_from=None,
+                                ),
+                            ],
+                            published_at=datetime.datetime(
+                                2024, 6, 24, 13, 19, 16
+                            ),
+                            related_cves=["CVE-2022-12345"],
+                            related_launchpad_bugs=[],
+                        ),
+                    ],
+                    vulnerability_data_published_at=datetime.datetime(
+                        2024, 6, 24, 13, 19, 16
+                    ),
+                    apt_updated_at=datetime.datetime(2024, 6, 24, 13, 19, 16),
+                ),
+            ),
+        ),
+    )
+    @mock.patch(M_PATH + "get_apt_cache_datetime")
+    @mock.patch(M_PATH + "get_release_info")
+    @mock.patch(M_PATH + "fetch_vulnerabilities_data")
+    @mock.patch(M_PATH + "query_installed_source_pkg_versions")
+    def test_parse_data(
+        self,
+        m_query_installed_source_pkg,
+        m_fetch_vulnerabilities_data,
+        m_get_release_info,
+        m_get_apt_cache_datetime,
+        vulnerabilities_data,
+        installed_pkgs_by_source,
+        usn_options,
+        expected_result,
+    ):
+        m_query_installed_source_pkg.return_value = installed_pkgs_by_source
+        m_fetch_vulnerabilities_data.return_value = vulnerabilities_data
+        m_get_release_info.return_value = mock.MagicMock(series="test")
+        m_get_apt_cache_datetime.return_value = datetime.datetime(
+            2024, 6, 24, 13, 19, 16
+        )
+        assert _vulnerabilities(usn_options, None) == expected_result
