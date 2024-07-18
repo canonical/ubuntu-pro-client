5b045f2d0e8855d176c2e36be1714d4235e297a9
Lucas Moura <lucas.moura@canonical.com>
2024-07-18 12:04:44 -0300

    bump version to 33.2

--- uaclient/version.py
+++ uaclient/version.py
@@ -15,7 +15,7 @@ from uaclient.defaults import CANDIDATE_CACHE_PATH, UAC_RUN_PATH
 from uaclient.exceptions import ProcessExecutionError
 from uaclient.system import subp
 
-__VERSION__ = "33.1"
+__VERSION__ = "33.2"
 PACKAGED_VERSION = "@@PACKAGED_VERSION@@"
 
 
