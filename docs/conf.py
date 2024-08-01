b7e205eb83aff2363fc5ad20035e1b787b3c44f4
Grant Orndorff <grant.orndorff@canonical.com>
2024-08-01 14:44:30 -0400

    don't copy console prompts

--- docs/conf.py
+++ docs/conf.py
@@ -150,3 +150,5 @@ notfound_context = {
     'title': 'Page not found',
     'body': '<h1>Page not found</h1>\n\n<p>Sorry, but the documentation page that you are looking for was not found.</p>\n<p>Documentation changes over time, and pages are moved around. We try to redirect you to the updated content where possible, but unfortunately, that didn\'t work this time (maybe because the content you were looking for does not exist in this version of the documentation).</p>\n<p>You can try to use the navigation to locate the content you\'re looking for, or search for a similar page.</p>\n',
 }
+
+copybutton_exclude = '.linenos, .gp'
