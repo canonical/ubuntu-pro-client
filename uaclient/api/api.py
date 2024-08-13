df49a0e49a24b3e584972be9ee7965f2415104d0
Dheyay <dheyay.desai@canonical.com>
2024-08-13 16:23:59 -0700

    api: u.pro.status.notices.v1
    Adds an api endpoint which returns list of active notices
    in the machine

--- uaclient/api/api.py
+++ uaclient/api/api.py
@@ -33,6 +33,7 @@ VALID_ENDPOINTS = [
     "u.pro.services.list.v1",
     "u.pro.status.enabled_services.v1",
     "u.pro.status.is_attached.v1",
+    "u.pro.status.notices.v1",
     "u.pro.token_info.v1",
     "u.apt_news.current_news.v1",
     "u.security.package_manifest.v1",
