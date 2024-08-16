5fded44a740bdb0f4aec2a7d974dc9231f112421
Lucas Moura <lucas.moura@canonical.com>
2024-08-16 14:43:23 -0300

    tests: change how to disable network on airgapped
    Change how we disable the network connection on the airgapped
    container, since the ufw approach was occasionally hanging in
    CI

--- features/steps/network.py
+++ features/steps/network.py
@@ -18,62 +18,14 @@ def block_access_to_address(context, website):
 def disable_internet_connection(context, machine_name=SUT):
     when_i_run_command(
         context,
-        "ufw default deny incoming",
+        "ip route del default",
         "with sudo",
         machine_name=machine_name,
     )
+
     when_i_run_command(
         context,
-        "ufw default deny outgoing",
-        "with sudo",
-        machine_name=machine_name,
-    )
-    when_i_run_command(
-        context,
-        "ufw allow from 10.0.0.0/8",
-        "with sudo",
-        machine_name=machine_name,
-    )
-    when_i_run_command(
-        context,
-        "ufw allow from 172.16.0.0/12",
-        "with sudo",
-        machine_name=machine_name,
-    )
-    when_i_run_command(
-        context,
-        "ufw allow from 192.168.0.0/16",
-        "with sudo",
-        machine_name=machine_name,
-    )
-    when_i_run_command(
-        context,
-        "ufw allow out to 10.0.0.0/8",
-        "with sudo",
-        machine_name=machine_name,
-    )
-    when_i_run_command(
-        context,
-        "ufw allow out to 172.16.0.0/12",
-        "with sudo",
-        machine_name=machine_name,
-    )
-    when_i_run_command(
-        context,
-        "ufw allow out to 192.168.0.0/16",
-        "with sudo",
-        machine_name=machine_name,
-    )
-    when_i_run_command(
-        context, "ufw allow ssh", "with sudo", machine_name=machine_name
-    )
-    # We expect DNS to be working, but don't really want to set a server up...
-    when_i_run_command(
-        context, "ufw allow out 53", "with sudo", machine_name=machine_name
-    )
-    when_i_run_command(
-        context,
-        "ufw --force enable",
+        "sed -i '$ a precedence ::ffff:0:0/96  100' /etc/gai.conf",
         "with sudo",
         machine_name=machine_name,
     )
