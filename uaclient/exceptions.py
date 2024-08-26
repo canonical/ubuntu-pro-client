7b5802d1a3902223c6c62c2394089e13716b7cfd
Lucas Moura <lucas.moura@canonical.com>
2024-08-26 15:25:33 -0300

    system: add logic to collect cpu type information
    We are adding the logic to collect the CPU type for attached
    users in the Pro client. We first identify the machine architecture
    and then we collect the CPU type for the appropriate given the
    architecture

--- uaclient/exceptions.py
+++ uaclient/exceptions.py
@@ -642,3 +642,7 @@ class SecurityIssueNotFound(UbuntuProError):
 
 class GPGKeyNotFound(UbuntuProError):
     _formatted_msg = messages.E_GPG_KEY_NOT_FOUND
+
+
+class UnknownProcessorType(UbuntuProError):
+    _formatted_msg = messages.E_UNKNOWN_PROCESSOR_TYPE
--- uaclient/messages/__init__.py
+++ uaclient/messages/__init__.py
@@ -2754,3 +2754,8 @@ E_INVALID_URL = FormattedNamedMessage(
     "invalid-url",
     t.gettext("Invalid URL: {url}"),
 )
+
+E_UNKNOWN_PROCESSOR_TYPE = FormattedNamedMessage(
+    "unknown-processor-type",
+    t.gettext("Unknown processor type: {processor_type}"),
+)
