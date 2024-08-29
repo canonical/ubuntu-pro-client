95d709dbda39b14e6bd00f20380a75dda17beed6
Renan Rodrigo <renanrodrigo@canonical.com>
2024-08-29 01:24:51 -0300

    docs: automatically generate the command entries for the rst docs
    Make the clidocgen script generate a commands.txt file to be used
    in the docs/references folder in the docs branch.
    
    Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

--- tools/clidocgen.py
+++ tools/clidocgen.py
@@ -10,7 +10,40 @@ sys.path.insert(0, ".")
 from uaclient.cli import COMMANDS, get_parser
 from uaclient.cli.commands import ProCommand
 
-VALID_TARGETS = ["manpage"]
+VALID_TARGETS = ["manpage", "rst"]
+
+GENERATED_DOC_HEADER = """\
+..
+   THIS DOCUMENTATION WAS AUTOMATICALLY GENERATED
+   Do not edit this document directly. Instead, edit the commands in the cli
+   source code, which can be found on the main branch of the git repo at
+   https://github.com/canonical/ubuntu-pro-client. The cli source code is
+   nested in the uaclient/cli folder. Description for the commands is found
+   in the uaclient/messages folder.
+
+Available commands
+==================
+
+The currently available commands are:
+
+{commands_list}
+
+"""
+COMMAND_LINK_TEMPLATE = "- `{command_entry}`_\n"
+
+RST_TEMPLATE = """\
+{command}
+{section_mark}
+
+**Usage:**
+
+``{usage_line}``
+
+**Description:**
+
+{description}
+
+"""
 
 MANPAGE_TEMPLATE = """\
 .TP
@@ -18,6 +51,17 @@ MANPAGE_TEMPLATE = """\
 {description}
 
 """
+
+
+def _build_rst_entry(command: ProCommand, section_mark: str = "="):
+    return RST_TEMPLATE.format(
+        command=command.parser.prog,
+        section_mark=section_mark * len(command.parser.prog),
+        usage_line=" ".join(
+            command.parser.format_usage()[len("usage: ") :].split()
+        ),
+        description=command.description,
+    )
 
 
 def _build_manpage_entry(command: ProCommand, indent: str = "") -> str:
@@ -34,6 +78,22 @@ def _build_manpage_entry(command: ProCommand, indent: str = "") -> str:
         usage_options=usage_options,
         description=command.description,
     )
+
+
+def _print_rst_section():
+    command_list = ""
+    for command in COMMANDS:
+        command_list += COMMAND_LINK_TEMPLATE.format(
+            command_entry=command.parser.prog
+        )
+
+    content = GENERATED_DOC_HEADER.format(commands_list=command_list)
+    for command in COMMANDS:
+        content += _build_rst_entry(command)
+        for subcommand in command.subcommands:
+            content += _build_rst_entry(subcommand, section_mark="-")
+
+    print(content)
 
 
 def _generate_manpage_section():
@@ -56,6 +116,9 @@ if __name__ == "__main__":
     if len(sys.argv) > 1:
         if sys.argv[1] == "manpage":
             _generate_manpage_section()
+            sys.exit()
+        if sys.argv[1] == "rst":
+            _print_rst_section()
             sys.exit()
 
     raise ValueError("call the script with one of: " + " ".join(VALID_TARGETS))
