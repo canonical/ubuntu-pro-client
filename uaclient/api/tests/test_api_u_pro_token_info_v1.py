dc0fe2e5eb0467c30907dee9bd1f02c3ad38e94e
Renan Rodrigo <renanrodrigo@canonical.com>
2025-02-10 16:48:59 -0300

    Revert "Update handling errors due to contract expiration"
    This reverts commit 94c044f39cb7740f7840e4de64f4fecac0332786.

--- uaclient/api/tests/test_api_u_pro_token_info_v1.py
+++ uaclient/api/tests/test_api_u_pro_token_info_v1.py
@@ -1,9 +1,6 @@
-import copy
-
 import mock
-import pytest
 
-from uaclient import exceptions, util
+from uaclient import util
 from uaclient.api.u.pro.token_info.v1 import (
     AccountInfo,
     ContractInfo,
@@ -47,15 +44,16 @@ RESPONSE_AVAILABLE_SERVICES = [
 ]
 
 
+@mock.patch(
+    "uaclient.api.u.pro.token_info.v1.get_available_resources",
+    return_value=RESPONSE_AVAILABLE_SERVICES,
+)
+@mock.patch(
+    "uaclient.api.u.pro.token_info.v1.get_contract_information",
+    return_value=RESPONSE_CONTRACT_INFO,
+)
 class TestTokenInfo:
-    @mock.patch(
-        "uaclient.api.u.pro.token_info.v1.get_available_resources",
-        return_value=RESPONSE_AVAILABLE_SERVICES,
-    )
-    @mock.patch(
-        "uaclient.api.u.pro.token_info.v1.get_contract_information",
-        return_value=RESPONSE_CONTRACT_INFO,
-    )
+
     def test_token_info_output(
         self, m_get_contract_information, m_get_available_resources, FakeConfig
     ):
@@ -65,12 +63,7 @@ class TestTokenInfo:
         )
         expected_info = TokenInfoResult(
             account=AccountInfo(id="some_id", name="Name"),
-            contract=ContractInfo(
-                id="some_id",
-                name="Name",
-                effective=None,
-                expires=util.parse_rfc3339_date("9999-12-31T00:00:00Z"),
-            ),
+            contract=ContractInfo(id="some_id", name="Name"),
             services=[
                 ServiceInfo(
                     name="livepatch",
@@ -82,57 +75,3 @@ class TestTokenInfo:
             ],
         )
         assert token_info == expected_info
-
-    @pytest.mark.parametrize(
-        "expected_error,expected_err_message,contract_field,date_value",
-        (
-            (
-                exceptions.TokenForbiddenExpired,
-                (
-                    'Contract "some_id" expired on December 31, 2019\n'
-                    "Visit https://ubuntu.com/pro/dashboard to manage "
-                    "contract tokens."
-                ),
-                "effectiveTo",
-                util.parse_rfc3339_date("2019-12-31T00:00:00Z"),
-            ),
-            (
-                exceptions.TokenForbiddenNotYet,
-                (
-                    'Contract "some_id" is not effective until December 31, '
-                    "9999\n"
-                    "Visit https://ubuntu.com/pro/dashboard to manage "
-                    "contract tokens."
-                ),
-                "effectiveFrom",
-                util.parse_rfc3339_date("9999-12-31T00:00:00Z"),
-            ),
-        ),
-    )
-    @mock.patch(
-        "uaclient.api.u.pro.token_info.v1.get_available_resources",
-        return_value=RESPONSE_AVAILABLE_SERVICES,
-    )
-    @mock.patch("uaclient.api.u.pro.token_info.v1.get_contract_information")
-    def test_attach_forbidden_error(
-        self,
-        m_get_contract_information,
-        m_get_available_resources,
-        expected_error,
-        expected_err_message,
-        contract_field,
-        date_value,
-        capsys,
-        FakeConfig,
-    ):
-        exp_contract = copy.deepcopy(RESPONSE_CONTRACT_INFO)
-        exp_contract["contractInfo"][contract_field] = date_value
-        m_get_contract_information.return_value = exp_contract
-        cfg = FakeConfig()
-        try:
-            _get_token_info(
-                options=TokenInfoOptions(token="contract_token"), cfg=cfg
-            )
-        except expected_error as e:
-            err_message = str(e)
-            assert err_message == expected_err_message
