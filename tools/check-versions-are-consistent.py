ca2fcf7465a832bf1a35694156637b199d283101
Renan Rodrigo <renanrodrigo@canonical.com>
2025-04-29 16:59:48 -0300

    chore: check-versions-are-consistent can ignore devel versions
    Signed-off-by: Renan Rodrigo <renanrodrigo@canonical.com>

--- tools/check-versions-are-consistent.py
+++ tools/check-versions-are-consistent.py
@@ -13,16 +13,17 @@ changelog_version = (
     .strip()
 )
 
+# We care about consistent versioning when releasing,
+# skip checking development versions
+if "+devel" in changelog_version:
+    sys.exit(0)
+
 # remove tilde and suffix of changelog_version if present
 # typical version strings:
 # GH: 32.3ubuntu1~1.gbp761c11~noble1 -> 32.3
 # backports: 32.3~22.04 -> 32.3
 # ppa test builds: 32.3~22.04~ppa1 -> 32.3
-# daily ppa: 1:1+devel-35-3475~gc001b39f~ubuntu24.10.1 -> 35
-#
-# focal and earlier don't have `removeprefix`
-if changelog_version.startswith("1:1+devel-"):
-    changelog_version = changelog_version[10:]
+
 m = re.match(r"(\d+(\.\d+)*)", changelog_version)
 if m:
     base_changelog_version = m.group()
