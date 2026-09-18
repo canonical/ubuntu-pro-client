0b83775f348b8fa6ac086820838bf6e573d61efa
Spencer Runde <spencer.runde@canonical.com>
2026-09-18 13:47:06 -0500

    refactor: add typed feature machine types

---
+++ features/machine_types.py
@@ -0,0 +1,43 @@
+from enum import Enum
+
+from pycloudlib.cloud import ImageType  # type: ignore
+
+
+class MachineType(Enum):
+    """Supported machine backends and cloud image types."""
+
+    AWS_GENERIC = ("aws.generic", "aws", ImageType.GENERIC)
+    AWS_PRO = ("aws.pro", "aws", ImageType.PRO)
+    AWS_PRO_FIPS = ("aws.pro-fips", "aws", ImageType.PRO_FIPS)
+    AZURE_GENERIC = ("azure.generic", "azure", ImageType.GENERIC)
+    AZURE_PRO = ("azure.pro", "azure", ImageType.PRO)
+    AZURE_PRO_FIPS = ("azure.pro-fips", "azure", ImageType.PRO_FIPS)
+    GCP_GENERIC = ("gcp.generic", "gcp", ImageType.GENERIC)
+    GCP_PRO = ("gcp.pro", "gcp", ImageType.PRO)
+    GCP_PRO_FIPS = ("gcp.pro-fips", "gcp", ImageType.PRO_FIPS)
+    LXD_CONTAINER = ("lxd-container", "lxd-container", None)
+    LXD_VM = ("lxd-vm", "lxd-vm", None)
+    WSL = ("wsl", "wsl", None)
+
+    def __new__(cls, value, cloud_name, image_type):
+        member = object.__new__(cls)
+        member._value_ = value
+        return member
+
+    def __init__(self, value, cloud_name, image_type):
+        self.cloud_name = cloud_name  # type: str
+        self.image_type = image_type  # type: ImageType
+
+    @classmethod
+    def from_string(cls, value: str) -> "MachineType":
+        try:
+            return cls(value)  # type: ignore
+        except ValueError:
+            raise ValueError("Unsupported machine type: {}".format(value))
+
+    @property
+    def is_pro_image(self):
+        return self.image_type in (
+            ImageType.PRO,
+            ImageType.PRO_FIPS,
+        )
